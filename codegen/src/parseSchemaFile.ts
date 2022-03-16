488763fb023b44df7db153863d18a5227bf4de45
Tejas Kumar <tejas@tejas.qa>
2022-03-16 17:56:01 +0100

    Clarify why we don't throw in `handleParsingError`

--- codegen/src/parseSchemaFile.ts
+++ codegen/src/parseSchemaFile.ts
@@ -6,6 +6,6 @@ export const parseSchemaFile = (input: string) => {
     return xataDatabaseSchema.parse(JSON.parse(input));
   } catch (err) {
     handleParsingError(err);
-    throw err; // ^ runs process.exit(1) if successful. If not, let's throw the error.
+    throw err; // ^ runs process.exit(1) if successful. If not, let's throw the error because if we don't, then this function would return its type | undefined and we'd have to optionally chain and account for potential undefined returns wherever we use it.
   }
 };
