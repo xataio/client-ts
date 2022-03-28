5be922efb0beb01628537222b64dc50007a3ac6f
Alexis Rico <sferadev@gmail.com>
2022-03-28 09:55:25 +0200

    Fix tests
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- client/src/schema/index.ts
+++ client/src/schema/index.ts
@@ -1,3 +1,5 @@
 export * from './operators';
 export type { XataRecord } from './record';
-export { Repository, RestRepository } from './repository';
+export { Repository, RestRepository, RestRespositoryFactory, BaseClient } from './repository';
+export type { XataClientOptions } from './repository';
+export { Query } from './query';
