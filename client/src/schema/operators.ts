237d88f125a0f859c8b865b637f187bb2b55d778
Alberto Gimeno <gimenete@gmail.com>
2022-04-01 22:58:48 +0200

    Adds some documentation

--- client/src/schema/operators.ts
+++ client/src/schema/operators.ts
@@ -2,23 +2,75 @@ import { Constraint } from './filters';
 
 type ComparableType = number | Date;
 
+/**
+ * Operator to restrict results to only values that are greater than the given value.
+ */
 export const gt = <T extends ComparableType>(value: T): Constraint<T> => ({ $gt: value });
+
+/**
+ * Operator to restrict results to only values that are greater than or equal to the given value.
+ */
 export const ge = <T extends ComparableType>(value: T): Constraint<T> => ({ $ge: value });
+/**
+ * Operator to restrict results to only values that are greater than or equal to the given value.
+ */
 export const gte = <T extends ComparableType>(value: T): Constraint<T> => ({ $ge: value });
+/**
+ * Operator to restrict results to only values that are lower than the given value.
+ */
 export const lt = <T extends ComparableType>(value: T): Constraint<T> => ({ $lt: value });
+/**
+ * Operator to restrict results to only values that are lower than or equal to the given value.
+ */
 export const lte = <T extends ComparableType>(value: T): Constraint<T> => ({ $le: value });
+/**
+ * Operator to restrict results to only values that are lower than or equal to the given value.
+ */
 export const le = <T extends ComparableType>(value: T): Constraint<T> => ({ $le: value });
+/**
+ * Operator to restrict results to only values that are not null.
+ */
 export const exists = (column: string): Constraint<string> => ({ $exists: column });
+/**
+ * Operator to restrict results to only values that are null.
+ */
 export const notExists = (column: string): Constraint<string> => ({ $notExists: column });
+/**
+ * Operator to restrict results to only values that start with the given prefix.
+ */
 export const startsWith = (value: string): Constraint<string> => ({ $startsWith: value });
+/**
+ * Operator to restrict results to only values that end with the given suffix.
+ */
 export const endsWith = (value: string): Constraint<string> => ({ $endsWith: value });
+/**
+ * Operator to restrict results to only values that match the given pattern.
+ */
 export const pattern = (value: string): Constraint<string> => ({ $pattern: value });
+/**
+ * Operator to restrict results to only values that are equal to the given value.
+ */
 export const is = <T>(value: T): Constraint<T> => ({ $is: value });
+/**
+ * Operator to restrict results to only values that are not equal to the given value.
+ */
 export const isNot = <T>(value: T): Constraint<T> => ({ $isNot: value });
+/**
+ * Operator to restrict results to only values that contain the given value.
+ */
 export const contains = <T>(value: T): Constraint<T> => ({ $contains: value });
 
 // TODO: these can only be applied to columns of type "multiple"
+/**
+ * Operator to restrict results to only arrays that include the given value.
+ */
 export const includes = (value: string): Constraint<string> => ({ $includes: value });
+/**
+ * Operator to restrict results to only arrays that include a value matching the given substring.
+ */
 export const includesSubstring = (value: string): Constraint<string> => ({ $includesSubstring: value });
+/**
+ * Operator to restrict results to only arrays that include a value matching the given pattern.
+ */
 export const includesPattern = (value: string): Constraint<string> => ({ $includesPattern: value });
 export const includesAll = (value: string): Constraint<string> => ({ $includesAll: value });
--- client/src/schema/record.ts
+++ client/src/schema/record.ts
@@ -1,12 +1,40 @@
 import { Selectable } from './selection';
 
+/**
+ * Represents a persisted record from the database.
+ */
 export interface XataRecord {
+  /**
+   * Unique id of this record.
+   */
   id: string;
+  /**
+   * Metadata of this record.
+   */
   xata: {
+    /**
+     * Number that is increased every time the record is updated.
+     */
     version: number;
   };
 
+  /**
+   * Retrieves a refreshed copy of the current record from the database.
+   */
   read(): Promise<this>;
+
+  /**
+   * Performs a partial update of the current record. On success a new object is
+   * returned and the current object is not mutated.
+   * @param data The columns and their values that have to be updated.
+   * @returns A new record containing the latest values for all the columns of the current record.
+   */
   update(data: Partial<Selectable<this>>): Promise<this>;
+
+  /**
+   * Performs a deletion of the current record in the database.
+   *
+   * @throws If the record was already deleted or if an error happened while performing the deletion.
+   */
   delete(): Promise<void>;
 }
