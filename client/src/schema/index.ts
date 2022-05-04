1fce619dea04581967c7df68c053e90b6bbb72c9
Alexis Rico <sferadev@gmail.com>
2022-05-04 10:23:40 +0200

    Fix exports
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- client/src/schema/index.ts
+++ client/src/schema/index.ts
@@ -1,6 +1,7 @@
 export * from './operators';
 export * from './pagination';
 export { Query } from './query';
-export type { Identifiable, isIdentifiable, isXataRecord, XataRecord } from './record';
+export { isIdentifiable, isXataRecord } from './record';
+export type { Identifiable, XataRecord } from './record';
 export { BaseClient, Repository, RestRepository, RestRespositoryFactory } from './repository';
 export type { XataClientOptions } from './repository';
