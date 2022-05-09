35bfd83f08961be3e40b57c3ffb2f6280359ac59
Alexis Rico <sferadev@gmail.com>
2022-05-09 10:02:04 +0200

    Improve nullable and record selection
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- client/src/schema/selection.spec.ts
+++ client/src/schema/selection.spec.ts
@@ -16,7 +16,27 @@ interface User {
   full_name: string;
   address?: { street?: string | null; zipcode?: number | null } | null;
   team?: TeamRecord | null;
-  owner: UserRecord;
+  partner: UserRecord;
+  settings?: {
+    theme?: 'light' | 'dark';
+    language?: 'en' | 'de' | 'fr';
+    signin?: {
+      email?: string | null;
+      github?: string | null;
+    };
+  } | null;
+  nonNullable: {
+    nested: {
+      value: number;
+      deep: {
+        depths: {
+          of: {
+            numbers: number[];
+          };
+        };
+      };
+    };
+  };
 }
 
 type UserRecord = User & XataRecord;
@@ -53,84 +73,182 @@ const invalidLabelsValue: ValueAtColumn<TeamRecord, 'labels'> = [1];
 //                            SelectedRecordPick<O, Key>                        //
 // ---------------------------------------------------------------------------- //
 
-function selectedRecordPickTest1(selectedUserBaseRecord: SelectedPick<UserRecord, ['*']>) {
-  selectedUserBaseRecord.id;
-  selectedUserBaseRecord.read();
-  selectedUserBaseRecord.full_name;
-  selectedUserBaseRecord.address?.street;
+function test1(user: SelectedPick<UserRecord, ['*']>) {
+  user.id;
+  user.read();
+  user.full_name;
+  user.address?.street;
 
   // @ts-expect-error
-  selectedUserBaseRecord.team.id;
-  selectedUserBaseRecord.team?.id;
-  selectedUserBaseRecord.team?.read();
+  user.team.id;
+  user.team?.id;
+  user.team?.read();
   // @ts-expect-error
-  selectedUserBaseRecord.team?.name;
+  user.team?.name;
 
-  selectedUserBaseRecord.owner.id;
-  selectedUserBaseRecord.owner.read();
+  user.partner.id;
+  user.partner.read();
   // @ts-expect-error
-  selectedUserBaseRecord.owner.full_name;
+  user.partner.full_name;
+
+  user.team = null;
+  // @ts-expect-error
+  user.partner = null;
 }
 
-function selectedRecordPickTest2(selectedUserFullRecord: SelectedPick<UserRecord, ['*', 'team.*']>) {
-  selectedUserFullRecord.id;
-  selectedUserFullRecord.read();
-  selectedUserFullRecord.full_name;
-  selectedUserFullRecord.team?.id;
-  selectedUserFullRecord.team?.read();
-  selectedUserFullRecord.team?.name;
-  selectedUserFullRecord.team?.owner;
-  selectedUserFullRecord.team?.owner?.id;
-  selectedUserFullRecord.team?.owner?.read();
+function test2(user: SelectedPick<UserRecord, ['*', 'team.*']>) {
+  user.id;
+  user.read();
+  user.full_name;
+  user.team?.id;
+  user.team?.read();
+  user.team?.name;
+  user.team?.owner;
+  user.team?.owner?.id;
+  user.team?.owner?.read();
   // @ts-expect-error
-  selectedUserFullRecord.team?.owner?.full_name;
+  user.team?.owner?.full_name;
 
   // @ts-expect-error
-  selectedUserFullRecord.full_name = null;
-  selectedUserFullRecord.email = null;
-  selectedUserFullRecord.email = '';
+  user.full_name = null;
+  user.email = null;
+  user.email = '';
   // @ts-expect-error
-  selectedUserFullRecord.email = 2;
-  if (selectedUserFullRecord.team) {
+  user.email = 2;
+  if (user.team) {
     // @ts-expect-error
-    selectedUserFullRecord.team.name = null;
-    selectedUserFullRecord.team.labels = null;
-    selectedUserFullRecord.team.labels = ['foo'];
+    user.team.name = null;
+    user.team.labels = null;
+    user.team.labels = ['foo'];
     // @ts-expect-error
-    selectedUserFullRecord.team.labels = [1];
+    user.team.labels = [1];
   }
 }
 
-function selectedRecordPickTest3(selectedUserNestedRecord: SelectedPick<UserRecord, ['team.owner.*']>) {
-  selectedUserNestedRecord.id;
-  selectedUserNestedRecord.read();
+function test3(user: SelectedPick<UserRecord, ['team.owner.*']>) {
+  user.id;
+  user.read();
   // @ts-expect-error
-  selectedUserNestedRecord.full_name;
-  selectedUserNestedRecord.team?.id;
-  selectedUserNestedRecord.team?.read();
+  user.full_name;
+  user.team?.id;
+  user.team?.read();
   // @ts-expect-error
-  selectedUserNestedRecord.team?.name;
-  selectedUserNestedRecord.team?.owner?.id;
-  selectedUserNestedRecord.team?.owner?.read();
-  selectedUserNestedRecord.team?.owner?.full_name;
+  user.team?.name;
+  user.team?.owner?.id;
+  user.team?.owner?.read();
+  user.team?.owner?.full_name;
 }
 
-function selectedRecordPickTest4(selectedUserNestedRecord: SelectedPick<UserRecord, ['team.owner.address']>) {
-  selectedUserNestedRecord.id;
-  selectedUserNestedRecord.read();
+function test4(user: SelectedPick<UserRecord, ['team.owner.address']>) {
+  user.id;
+  user.read();
   // @ts-expect-error
-  selectedUserNestedRecord.full_name;
-  selectedUserNestedRecord.team?.id;
-  selectedUserNestedRecord.team?.read();
+  user.full_name;
+  user.team?.id;
+  user.team?.read();
   // @ts-expect-error
-  selectedUserNestedRecord.team?.name;
-  selectedUserNestedRecord.team?.owner?.id;
-  selectedUserNestedRecord.team?.owner?.read();
+  user.team?.name;
+  user.team?.owner?.id;
+  user.team?.owner?.read();
   // @ts-expect-error
-  selectedUserNestedRecord.team?.owner?.full_name;
-  selectedUserNestedRecord.team?.owner?.address;
-  selectedUserNestedRecord.team?.owner?.address?.street;
-  selectedUserNestedRecord.team?.owner?.address?.zipcode;
+  user.team?.owner?.full_name;
+  user.team?.owner?.address;
+  user.team?.owner?.address?.street;
+  user.team?.owner?.address?.zipcode;
+}
+
+function test5(user: SelectedPick<UserRecord, ['settings', 'nonNullable']>) {
+  user.id;
+  user.read();
+  // @ts-expect-error
+  user.full_name;
+  user.settings;
+  user.settings?.theme;
+  user.settings?.language;
+  user.settings?.signin;
+  user.settings?.signin?.email;
+  user.settings?.signin?.github;
+  // @ts-expect-error
+  user.settings?.id;
+  // @ts-expect-error
+  user.settings?.read();
+  // @ts-expect-error
+  user.settings?.signin?.id;
+  user.settings = null;
+  user.nonNullable.nested.value = 2;
+  // @ts-expect-error
+  user.nonNullable = null;
+  // @ts-expect-error
+  user.nonNullable.nested.value = null;
+}
+
+function test6(user: SelectedPick<UserRecord, ['settings.*', 'nonNullable.*']>) {
+  user.id;
+  user.read();
+  // @ts-expect-error
+  user.full_name;
+  user.settings;
+  user.settings?.theme;
+  user.settings?.language;
+  user.settings?.signin;
+  user.settings?.signin?.email;
+  user.settings?.signin?.github;
+  // @ts-expect-error
+  user.settings?.id;
+  // @ts-expect-error
+  user.settings?.read();
+  // @ts-expect-error
+  user.settings?.signin?.id;
+  user.nonNullable.nested.value = 2;
+  user.nonNullable.nested.deep.depths.of.numbers = [1, 2, 3];
+  // @ts-expect-error
+  user.nonNullable.nested.deep.depths.of.numbers = 'invalid-string';
+
+  user.settings = null;
+  // @ts-expect-error
+  user.nonNullable = null;
+  // @ts-expect-error
+  user.nonNullable.nested.value = null;
+}
+
+function test7(user: SelectedPick<UserRecord, ['partner', 'team']>) {
+  user.partner;
+  user.partner.id;
+  user.partner.read();
+  user.partner.full_name;
+  // @ts-expect-error
+  user.partner.full_name = null;
+  user.partner.address;
+  // @ts-expect-error
+  user.team.id;
+  user.team?.id;
+  // @ts-expect-error
+  user.team.read();
+  user.team?.read();
+
+  // @ts-expect-error
+  user.partner = null;
+  user.team = null;
+}
+
+function test8(user: SelectedPick<UserRecord, ['partner.*', 'team.*']>) {
+  user.partner;
+  user.partner.id;
+  user.partner.read();
+  user.partner.full_name;
+  // @ts-expect-error
+  user.partner.full_name = null;
+  user.partner.address;
+  // @ts-expect-error
+  user.team.id;
+  user.team?.id;
+  // @ts-expect-error
+  user.team.read();
+  user.team?.read();
+
+  // @ts-expect-error
+  user.partner = null;
+  user.team = null;
 }
 
 test('fake test', () => {
