c08703e0c98832e65467abe86e1218c91dfe20ae
Alexis Rico <sferadev@gmail.com>
2022-05-14 19:46:26 +0200

    Add sort filter base

--- client/src/schema/sorting.ts
+++ client/src/schema/sorting.ts
@@ -1,5 +1,5 @@
 import { isObject, isString } from '../util/lang';
-import { SingleOrArray, Values } from '../util/types';
+import { SingleOrArray, StringKeys, Values } from '../util/types';
 import { XataRecord } from './record';
 import { SelectableColumn } from './selection';
 
@@ -9,11 +9,15 @@ export type SortFilterExtended<T extends XataRecord> = {
   direction?: SortDirection;
 };
 
-export type SortFilter<T extends XataRecord> = SelectableColumn<T> | SortFilterExtended<T>;
+export type SortFilter<T extends XataRecord> = SelectableColumn<T> | SortFilterExtended<T> | SortFilterBase<T>;
+
+type SortFilterBase<T extends XataRecord> = {
+  [Key in StringKeys<T>]: SortDirection;
+};
 
 export type ApiSortFilter<T extends XataRecord> = SingleOrArray<
   Values<{
-    [key in SelectableColumn<T>]: { [K in key]: SortDirection };
+    [Key in SelectableColumn<T>]: { [K in Key]: SortDirection };
   }>
 >;
 
@@ -21,8 +25,12 @@ export function isSortFilterString<T extends XataRecord>(value: any): value is S
   return isString(value);
 }
 
+export function isSortFilterBase<T extends XataRecord>(filter: SortFilter<T>): filter is SortFilterBase<T> {
+  return isObject(filter) && Object.values(filter).every((value) => value === 'asc' || value === 'desc');
+}
+
 export function isSortFilterObject<T extends XataRecord>(filter: SortFilter<T>): filter is SortFilterExtended<T> {
-  return isObject(filter) && filter.column !== undefined;
+  return isObject(filter) && !isSortFilterBase(filter) && filter.column !== undefined;
 }
 
 export function buildSortFilter<T extends XataRecord>(filter: SingleOrArray<SortFilter<T>>): ApiSortFilter<T> {
@@ -30,6 +38,8 @@ export function buildSortFilter<T extends XataRecord>(filter: SingleOrArray<Sort
     return { [filter]: 'asc' } as { [key in SelectableColumn<T>]: SortDirection };
   } else if (Array.isArray(filter)) {
     return filter.map((item) => buildSortFilter(item)) as { [key in SelectableColumn<T>]: SortDirection }[];
+  } else if (isSortFilterBase(filter)) {
+    return filter as { [key in SelectableColumn<T>]: SortDirection };
   } else if (isSortFilterObject(filter)) {
     return { [filter.column]: filter.direction ?? 'asc' } as { [key in SelectableColumn<T>]: SortDirection };
   } else {
