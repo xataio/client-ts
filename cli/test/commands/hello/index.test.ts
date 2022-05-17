2f0de48ceee1e2d7050140e4843b4e328c96b3c4
Alberto Gimeno <gimenete@gmail.com>
2022-05-17 10:51:34 +0200

    Fix test

--- cli/test/commands/hello/index.test.ts
+++ cli/test/commands/hello/index.test.ts
@@ -1,10 +1,5 @@
-import { expect, test } from '@oclif/test';
+import { test } from '@oclif/test';
 
 describe('branches list', () => {
-  test
-    .stdout()
-    .command(['branches:list'])
-    .it('runs branches list', (ctx) => {
-      expect(ctx.stdout).to.contain('hello world');
-    });
+  test.command(['branches:list']).exit(2).it('Exits with error');
 });
