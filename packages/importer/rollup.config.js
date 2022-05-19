b54aebc5be756cc38476ad499d6194db0e02397e
Alexis Rico <sferadev@gmail.com>
2022-05-19 08:50:24 +0200

    Revert "Add internal dependencies"
    This reverts commit d5a3ce6436fc2491b27f4e08ffe8a0e9d26f6c32.

--- packages/importer/rollup.config.js
+++ packages/importer/rollup.config.js
@@ -1,15 +1,14 @@
 import autoExternal from 'rollup-plugin-auto-external';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
-import internal from 'rollup-plugin-internal';
 import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
 import shebang from 'rollup-plugin-preserve-shebang';
+import globals from 'rollup-plugin-node-globals';
 
 export default [
   {
     input: 'src/index.ts',
-    plugins: [esbuild(), autoExternal(), internal(['@xata.io/client'])],
+    plugins: [esbuild()],
     output: [
       {
         file: `dist/index.cjs`,
@@ -38,6 +37,6 @@ export default [
       format: 'es',
       banner: '#!/usr/bin/env node'
     },
-    plugins: [shebang(), globals(), builtins(), esbuild(), autoExternal(), internal(['@xata.io/client'])]
+    plugins: [shebang(), globals(), builtins(), esbuild(), autoExternal()]
   }
 ];
