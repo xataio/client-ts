12f4f6edd19ef4e465af74f0e8d88a5ee3938820
Alberto Gimeno <gimenete@gmail.com>
2022-05-23 15:40:31 +0200

    Create branch

--- cli/src/commands/branches/create.ts
+++ cli/src/commands/branches/create.ts
@@ -1,15 +1,52 @@
+import { Flags } from '@oclif/core';
 import { BaseCommand } from '../../base.js';
+import { parseDatabaseURL } from '../../defaults.js';
 
 export default class BranchesCreate extends BaseCommand {
   static description = 'Create a branch';
 
   static examples = [];
 
-  static flags = {};
+  static flags = {
+    workspace: Flags.string({
+      description: 'Workspace id the database belongs to'
+    }),
+    database: Flags.string({
+      description: 'Database the branch will belongs to'
+    }),
+    from: Flags.string({
+      description: 'Branch name to branch off from'
+    })
+  };
 
-  static args = [];
+  static args = [{ name: 'branch', description: 'The new branch name', required: true }];
 
-  async run(): Promise<void> {
-    this.error('To be done');
+  static enableJsonFlag = true;
+
+  async run(): Promise<void | unknown> {
+    const { args, flags } = await this.parse(BranchesCreate);
+    const { branch } = args;
+
+    const defaults = parseDatabaseURL();
+    const workspace = flags.workspace || defaults.workspace;
+    const database = flags.database || defaults.database;
+
+    if (!workspace) {
+      return this.error('Could not find workspace id. Please set XATA_DATABASE_URL or use the --workspace flag.');
+    }
+    if (!database) {
+      return this.error('Could not find database name. Please set XATA_DATABASE_URL or use the --database flag.');
+    }
+    if (!branch) {
+      return this.error('Please, specify a branch name');
+    }
+
+    const xata = await this.getXataClient();
+
+    const result = await xata.branches.createBranch(workspace, database, branch, flags.from);
+
+    if (this.jsonEnabled()) return result;
+
+    this.log(`Branch ${branch} successfully created`);
   }
 }
