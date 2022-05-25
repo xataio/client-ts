47f9ab1c10c953d8ffce4988711e5b89d971f28e
Alberto Gimeno <gimenete@gmail.com>
2022-05-25 15:29:41 +0200

    CLI codegen

--- cli/src/base.ts
+++ cli/src/base.ts
@@ -5,7 +5,19 @@ import chalk from 'chalk';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 import table from 'text-table';
+import { z } from 'zod';
 import { readAPIKey } from './key.js';
+import { cosmiconfigSync } from 'cosmiconfig';
+
+export const userConfig = z.object({
+  codegen: z.optional(
+    z.object({
+      output: z.string()
+    })
+  )
+});
+
+export type UserConfig = z.infer<typeof userConfig>;
 
 export abstract class BaseCommand extends Command {
   // Date formatting is not consistent across locales and timezones, so we need to set the locale and timezone for unit tests.
@@ -13,8 +25,24 @@ export abstract class BaseCommand extends Command {
   locale: string | undefined = undefined;
   timeZone: string | undefined = undefined;
 
+  userConfig: UserConfig | undefined;
+
   async init() {
     dotenv.config();
+
+    const search = cosmiconfigSync('xata').search();
+    if (search) {
+      const result = userConfig.safeParse(search.config);
+      if (result.success) {
+        this.userConfig = result.data;
+      } else {
+        this.warn(`The configuration file ${search.filepath} was found, but could not be parsed:`);
+
+        for (const error of result.error.errors) {
+          this.warn(`  [${error.code}] ${error.message} at "${error.path.join('.')}"`);
+        }
+      }
+    }
   }
 
   async getXataClient() {
---
+++ cli/src/commands/codegen/index.ts
@@ -0,0 +1,54 @@
+import { generateFromContext } from '@xata.io/codegen';
+import { mkdir, writeFile } from 'fs/promises';
+import fetch from 'node-fetch';
+import path, { dirname, extname, relative } from 'path';
+import { BaseCommand } from '../../base.js';
+
+const langauges: Record<string, 'javascript' | 'typescript'> = {
+  '.js': 'javascript',
+  '.mjs': 'javascript',
+  '.ts': 'typescript'
+};
+
+export default class Codegen extends BaseCommand {
+  static description = 'Generate code form the current schema';
+
+  static examples = [];
+
+  static flags = {};
+
+  static args = [{ name: 'output', description: 'The output file to generate', required: false }];
+
+  async run(): Promise<void> {
+    const { args } = await this.parse(Codegen);
+
+    const output = args.output || this.userConfig?.codegen?.output;
+
+    if (!output) {
+      return this.error('Please, specify an output file as an argument or configure it in your config file');
+    }
+
+    const ext = extname(output);
+    const dir = dirname(output);
+    const language = langauges[ext];
+    if (!language) {
+      return this.error(
+        `Cannot generate code for a file with extension ${ext}. Please use one of the following extensions: ${Object.keys(
+          langauges
+        ).join(', ')}`
+      );
+    }
+
+    const result = await generateFromContext('typescript', { fetchImpl: fetch });
+    const code = result.transpiled;
+    const declarations = result.declarations;
+
+    await mkdir(dir, { recursive: true });
+    await writeFile(output, code);
+    if (declarations && language != 'typescript') {
+      await writeFile(path.join(dir, 'types.d.ts'), declarations);
+    }
+
+    this.log(`Your XataClient is generated at ./${relative(process.cwd(), output)}`);
+  }
+}
