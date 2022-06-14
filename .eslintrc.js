1ee4b4389bc68c5f4d2900143114aca6ade4276f
Alexis Rico <sferadev@gmail.com>
2022-06-14 12:49:33 +0200

    Fix linter runner
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
   },
   overrides: [
     {
