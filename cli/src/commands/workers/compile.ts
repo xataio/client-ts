94ce03bfe0541458562504c0f34fb8acb1e59ebf
Alexis Rico <sferadev@gmail.com>
2022-06-15 17:05:12 +0200

    Add chokidar for glob listing
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/commands/workers/compile.ts
+++ cli/src/commands/workers/compile.ts
@@ -1,11 +1,34 @@
+import { Flags } from '@oclif/core';
 import { BaseCommand } from '../../base.js';
+import chokidar from 'chokidar';
 
 export default class WorkersCompile extends BaseCommand {
   static description = 'Extract and compile xata workers';
 
-  static flags = {};
+  static flags = {
+    watch: Flags.boolean({
+      char: 'w',
+      description: 'Watch for changes and recompile',
+      default: false
+    })
+  };
 
   async run(): Promise<void> {
-    this.log('hello world');
+    const { flags } = await this.parse(WorkersCompile);
+
+    const watcher = chokidar.watch('./**/*.ts', {
+      ignored: [/(^|[/\\])\../, 'dist/*', 'node_modules/*']
+    });
+
+    watcher
+      .on('add', (path) => this.#compile(path))
+      .on('change', (path) => this.#compile(path))
+      .on('ready', async () => {
+        if (!flags.watch) await watcher.close();
+      });
+  }
+
+  async #compile(file: string): Promise<void> {
+    this.log(`Compiling ${file}`);
   }
 }
