3babe8cc8d2fe79d441cd1e38996d19ba0e0dd0c
Alexis Rico <sferadev@gmail.com>
2022-06-16 12:28:21 +0200

    Update comments from meeting
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/workers/xata-worker.ts
+++ cli/src/workers/xata-worker.ts
@@ -1,11 +1,12 @@
 type Worker = unknown;
 
-export function xataWorker<T extends (...args: any[]) => any>(callback: T): Worker;
-export function xataWorker<T extends (...args: any[]) => any>(name: string, callback: T): Worker;
-export function xataWorker<T extends (...args: any[]) => any>(a: T | string, b?: T): Worker {
+export function xataWorker<T extends (xata: 'foo', ...args: any[]) => any>(callback: T): Worker;
+export function xataWorker<T extends (xata: 'foo', ...args: any[]) => any>(name: string, callback: T): Worker;
+
+export function xataWorker<T extends (xata: 'foo', ...args: any[]) => any>(a: T | string, b?: T): Worker {
   console.log('xataWorker', a, b);
 
-  // If env is development: return the worker fn directly
+  // If env is development: return a fn that call http://localhost:8080/workers?worker=name-prefix-1241hg2&args=${b}
   // If env is production: return a fn that calls https://workers.xata.io?worker=name-prefix-1241hg2&args=${b}
 
   throw new Error('Not implemented');
@@ -14,7 +15,7 @@ export function xataWorker<T extends (...args: any[]) => any>(a: T | string, b?:
 export const foo = 'bar';
 
 /**
- xataWorker("name-prefix-1241hg2", ({ xata, ctx }) => {
-    return xata.db.teams.filter("name", ctx.name).getAll();
+ xataWorker("name-prefix", ({ xata }, ...args) => {
+    return xata.db.teams.filter("name", params.name).getAll();
  })
  */
