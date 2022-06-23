7cf9acd00421612886174a1bd0ee5bbdeeac2434
Alberto Gimeno <gimenete@gmail.com>
2022-06-23 23:05:53 +0200

    Clean things up and fix stuff

---
+++ packages/codegen/example/types.d.ts
@@ -0,0 +1,26 @@
+import { BaseClientOptions, XataRecord } from '@xata.io/client';
+export interface Team {
+  name?: string | null;
+  labels?: string[] | null;
+  owner?: UserRecord | null;
+}
+export declare type TeamRecord = Team & XataRecord;
+export interface User {
+  email?: string | null;
+  full_name?: string | null;
+  address?: {
+    street?: string | null;
+    zipcode?: number | null;
+  } | null;
+  team?: TeamRecord | null;
+}
+export declare type UserRecord = User & XataRecord;
+export declare type DatabaseSchema = {
+  teams: Team;
+  users: User;
+};
+declare const DatabaseClient: any;
+export declare class XataClient extends DatabaseClient<DatabaseSchema> {
+  constructor(options?: BaseClientOptions);
+}
+export {};
