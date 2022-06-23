a803ba54d004c4a42d059c84bccf0b0f6414ffcc
Alberto Gimeno <gimenete@gmail.com>
2022-06-23 18:46:10 +0200

    Deprecate codegen CLI

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,7 +1,4 @@
 export { generate } from './codegen';
-export { generateFromContext } from './api';
 export type { GenerateOptions, Language } from './codegen';
-export type { XataConfigSchema } from './config';
-export { parseConfigFile } from './config';
 export { parseSchemaFile } from './schema';
 export type { Column, Table, XataDatabaseSchema } from './schema';
