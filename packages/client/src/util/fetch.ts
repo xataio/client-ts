f50aa5a756372875d3bd4e621787881b5f8bffab
Alexis Rico <sferadev@gmail.com>
2022-08-16 11:35:23 +0200

    Update packages/client/src/util/fetch.ts

--- packages/client/src/util/fetch.ts
+++ packages/client/src/util/fetch.ts
@@ -6,7 +6,7 @@ export function getFetchImplementation(userFetch?: FetchImpl) {
   if (!fetchImpl) {
     /** @todo add a link after docs exist */
     throw new Error(
-      `Couldn't find \`fetch\`. Install a fetch implementation such as \`node-fetch\` and pass it explicitly to the XataClient or XataApiClient constructors or the getCurrentBranchXXX() functions`
+      `Couldn't find \`fetch\`. Install a fetch implementation such as \`node-fetch\` and pass it explicitly.`
     );
   }
   return fetchImpl;
