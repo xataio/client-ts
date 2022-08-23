9db8566dd1c520ff0b2ffadec986328fbca04c88
Alexis Rico <sferadev@gmail.com>
2022-08-23 10:46:38 +0200

    Allow running tests from staging and localhost
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { join } from 'path';
 import { vi } from 'vitest';
 import { BaseClient, CacheImpl, XataApiClient } from '../../packages/client/src';
+import { getHostUrl, HostProvider, isHostProviderAlias } from '../../packages/client/src/api/providers';
 import { XataClient } from '../../packages/codegen/example/xata';
 import { teamColumns, userColumns } from '../mock_data';
 
@@ -15,6 +16,7 @@ if (apiKey === '') throw new Error('XATA_API_KEY environment variable is not set
 const workspace = process.env.XATA_WORKSPACE ?? '';
 if (workspace === '') throw new Error('XATA_WORKSPACE environment variable is not set');
 
+const host = getProvider(process.env.XATA_API_PROVIDER);
 const fetch = vi.fn(realFetch);
 
 export type EnvironmentOptions = {
@@ -37,14 +39,16 @@ export type TestEnvironmentResult = {
 export async function setUpTestEnvironment(prefix: string, { cache }: EnvironmentOptions = {}) {
   const id = Math.round(Math.random() * 100000);
 
-  const api = new XataApiClient({ apiKey, fetch });
+  const api = new XataApiClient({ apiKey, fetch, host });
   const { databaseName: database } = await api.databases.createDatabase(
     workspace,
     `sdk-integration-test-${prefix}-${id}`
   );
 
+  const workspaceUrl = getHostUrl(host, 'workspaces').replace('{workspaceId}', workspace);
+
   const clientOptions = {
-    databaseURL: `https://${workspace}.xata.sh/db/${database}`,
+    databaseURL: `${workspaceUrl}/db/${database}`,
     branch: 'main',
     apiKey,
     fetch,
@@ -64,4 +68,12 @@ export async function setUpTestEnvironment(prefix: string, { cache }: Environmen
   const baseClient = new BaseClient(clientOptions);
 
   return { api, client, baseClient, clientOptions, database, workspace, cleanup };
+}
+
+function getProvider(provider = 'production'): HostProvider {
+  if (isHostProviderAlias(provider)) {
+    return provider;
+  }
+
+  return { main: provider, workspaces: provider };
 }
