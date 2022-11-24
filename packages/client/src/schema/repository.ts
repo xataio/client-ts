280450c218677f8c813bc3dfe8ad3ee214ad4dcf
Alexis Rico <sferadev@gmail.com>
2022-11-24 08:39:43 +0100

    Expose on summarize too
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/client/src/schema/repository.ts
+++ packages/client/src/schema/repository.ts
@@ -1782,6 +1782,7 @@ export class RestRepository<Record extends XataRecord>
           filter: cleanFilter(data.filter),
           sort: data.sort !== undefined ? buildSortFilter(data.sort) : undefined,
           columns: data.columns,
+          consistency: data.consistency,
           page: data.pagination?.size !== undefined ? { size: data.pagination?.size } : undefined,
           summaries,
           summariesFilter
