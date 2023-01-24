7f1a0668404dd396051f2b777b964a7899a0a33c
Alexis Rico <sferadev@gmail.com>
2023-01-24 12:57:09 +0100

    Add pull base
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/commands/pull/index.ts
+++ cli/src/commands/pull/index.ts
@@ -1,15 +1,45 @@
+import { Flags } from '@oclif/core';
+import { parseTablesFromCodegen } from '@xata.io/codegen';
+import { readFile } from 'fs/promises';
 import { BaseCommand } from '../../base.js';
 
 export default class Pull extends BaseCommand {
-  static description = 'Pull schema and migrations from a remote Xata branch and apply them locally';
+  static description = 'Push local migrations to a remote Xata branch';
 
   static examples = [];
 
   static flags = {
-    ...this.commonFlags
+    ...this.commonFlags,
+    ...this.databaseURLFlag
   };
 
-  static args = [];
+  static args = [
+    {
+      name: 'branch',
+      description: 'The remote branch to push to',
+      required: false
+    }
+  ];
 
-  async run() {}
+  async run() {
+    const { args, flags } = await this.parse(Pull);
+
+    const xata = await this.getXataClient();
+    const { workspace, region, database, branch } = await this.getParsedDatabaseURLWithBranch(
+      flags.db,
+      args.branch ?? 'main',
+      true
+    );
+
+    const { logs, meta } = await xata.api.migrations.getBranchSchemaHistory({
+      workspace,
+      region,
+      database,
+      branch,
+      page: { before: 'end', size: 200 }
+    });
+
+    this.log(`Found ${logs.length} migrations in ${branch}`);
+    this.log(`Meta: ${JSON.stringify(meta, null, 2)}`);
+  }
 }
