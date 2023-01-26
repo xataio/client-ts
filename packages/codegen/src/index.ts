dfc6bb7454e081196a427e8693682621e7fba9ac
Alexis Rico <sferadev@gmail.com>
2023-01-26 10:23:13 +0100

    Remove json5
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,5 +1,4 @@
 export { generate, isValidJavascriptTarget, javascriptTargets } from './codegen';
 export type { GenerateOptions, Language, ModuleType } from './codegen';
 export { parseSchemaFile } from './schema';
-export { parseTablesFromCodegen } from './parse';
 export type { Column, Table, XataDatabaseSchema } from './schema';
