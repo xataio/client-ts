7b34e7926fcbec7343762228a9fc3ff6f831e0f8
Alexis Rico <sferadev@gmail.com>
2023-01-26 14:47:13 +0100

    Implement branch command
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/commands/branch/index.ts
+++ cli/src/commands/branch/index.ts
@@ -1,7 +1,7 @@
 import { BaseCommand } from '../../base.js';
 
 export default class Branch extends BaseCommand {
-  static description = 'Checkout a remote branch or create a new local branch';
+  static description = 'List, create, or delete branches';
 
   static examples = [];
 
@@ -9,11 +9,54 @@ export default class Branch extends BaseCommand {
     ...this.commonFlags
   };
 
-  static args = [];
+  static args = [
+    {
+      name: 'branch',
+      description: 'The branch to create',
+      required: false
+    },
+    {
+      name: 'base',
+      description: 'The branch to base the new branch on',
+      required: false
+    }
+  ];
 
   static hidden = true;
 
   async run() {
-    // Nothing to see here, move along
+    const { args, flags } = await this.parse(Branch);
+
+    const xata = await this.getXataClient();
+    const { workspace, region, database } = await this.getParsedDatabaseURL(flags.db);
+
+    const { branches } = await xata.api.branches.getBranchList({
+      workspace,
+      region,
+      database
+    });
+
+    if (args.branch) {
+      const branch = branches.find((branch) => branch.name === args.branch);
+      if (branch) {
+        this.log(`Branch ${branch.name} already exists`);
+      } else {
+        await xata.api.branches.createBranch({
+          workspace,
+          region,
+          database,
+          branch: args.branch,
+          from: args.base ?? 'main'
+        });
+        this.log(`Branch ${args.branch} created`);
+      }
+
+      return;
+    }
+
+    if (this.jsonEnabled()) return branches;
+
+    const rows = branches.map((b) => [b.name, this.formatDate(b.createdAt)]);
+    this.printTable(['Name', 'Created at'], rows);
   }
 }
