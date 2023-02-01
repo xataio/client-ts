c745d5175b2c28ae3afcdb81792dfa02fbfdfcc3
Alexis Rico <sferadev@gmail.com>
2023-02-01 12:54:25 +0100

    Add rebase
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/commands/diff/index.ts
+++ cli/src/commands/diff/index.ts
@@ -9,11 +9,30 @@ export default class Diff extends BaseCommand {
     ...this.commonFlags
   };
 
-  static args = [];
+  static args = [
+    {
+      name: 'branch',
+      description: 'The branch to compare',
+      required: false
+    },
+    {
+      name: 'base',
+      description: 'The base branch to compare against',
+      required: false
+    }
+  ];
 
   static hidden = true;
 
   async run() {
-    // Nothing to see here, move along
+    const { args } = await this.parse(Diff);
+
+    if (args.branch && args.base) {
+      // Compare two remote branches
+    } else if (args.branch) {
+      // Compare the local branch with the remote branch
+    } else {
+      // Compare the local branch with the remote main branch
+    }
   }
 }
