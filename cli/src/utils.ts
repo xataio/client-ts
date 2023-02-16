ed381a14c255462074ddc44364c3f97235d93e3e
Richard Gill <richard@rgill.co.uk>
2023-02-16 13:27:34 +0000

    made sure front end backend regexes match

--- cli/src/utils.ts
+++ cli/src/utils.ts
@@ -8,9 +8,9 @@ export function reportBugURL(title: string) {
   )}`;
 }
 
-// https://stackoverflow.com/a/8829363
+// Same as xata apis (removed a single `\` to make it js compatible)
 const EMAIL_REGEX =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  /^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 export function isValidEmail(email: string) {
   return EMAIL_REGEX.test(email);
 }
