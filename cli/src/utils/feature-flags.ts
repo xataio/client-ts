54edea5c3a3ddcb9bba736b0df9b47983c008004
Alexis Rico <sferadev@gmail.com>
2023-03-03 15:17:13 +0100

    Remove old code
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/utils/feature-flags.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectConfig } from '../config';
-
-export function hasExperimentalFlag(config: ProjectConfig, flag: keyof NonNullable<ProjectConfig['experimental']>) {
-  return config.experimental?.[flag] === true;
-}
-
-export function hasRemoteFlag() {
-  // TODO: implement
-}
