eaed2bae713cd26d96e27a45fccead4f16912fec
Tudor Golubenco <tudor@xata.io>
2023-03-07 23:27:45 +0100

    Increase timeouts

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ process.env.FORCE_COLOR = '0';
 export default defineConfig({
   test: {
     // TODO: Parallelize tests and mark integration tests as long-running
-    testTimeout: 60000,
-    hookTimeout: 120000
+    testTimeout: 120000,
+    hookTimeout: 240000
   }
 });
