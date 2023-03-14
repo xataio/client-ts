92bb4db5a85388b9a4812ee30c89551cead9fdab
Alexis Rico <sferadev@gmail.com>
2023-03-14 12:37:38 +0100

    Updates
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/plugin-client-netlify/src/index.ts
+++ packages/plugin-client-netlify/src/index.ts
@@ -2,32 +2,33 @@ import type { OnPreBuild } from '@netlify/build';
 import { buildPreviewBranchName } from '@xata.io/client';
 
 export const onPreBuild: OnPreBuild = async function ({ netlifyConfig, utils }) {
-  if (netlifyConfig.build.environment.CONTEXT !== 'deploy-preview') {
+  const { CONTEXT: context, BRANCH: branch, XATA_PREVIEW: preview } = netlifyConfig.build.environment;
+
+  if (context !== 'deploy-preview') {
     console.log('Not a deploy preview, skipping Xata plugin');
     return;
   }
 
-  if (netlifyConfig.build.environment.XATA_PREVIEW !== 'netlify') {
+  if (preview !== 'netlify') {
     console.log('XATA_PREVIEW is not set to netlify, skipping Xata plugin');
     return;
   }
 
   console.log(JSON.stringify(netlifyConfig, null, 2));
 
-  const { stdout: gitHead, stderr } = await utils.run('git', ['rev-parse', 'HEAD']);
-  if (!gitHead || stderr) {
+  if (!branch) {
     console.log('No git HEAD found, skipping Xata plugin');
     return;
   }
 
-  const githubRegex = /^https?:\/\/(?:www\.)?github\.com\/(?<owner>[^/]+)\//;
-  const owner = netlifyConfig.build.environment.REPOSITORY_URL?.match(githubRegex)?.groups?.owner;
-  if (!owner) {
+  // TODO: get org from netlify config
+  const org = 'SferaDev';
+  if (!org) {
     console.log('No GitHub owner found, skipping Xata plugin');
     return;
   }
 
-  const previewBranch = buildPreviewBranchName({ org: owner, branch: gitHead });
+  const previewBranch = buildPreviewBranchName({ org, branch });
   netlifyConfig.build.environment.XATA_PREVIEW_BRANCH = previewBranch;
   console.log(`Xata preview branch set to ${previewBranch}`);
 };
