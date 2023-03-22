0da60b545d9b34b5c5a5f68079bc285d3bfdd1de
Alexis Rico <sferadev@gmail.com>
2023-03-22 11:30:23 +0100

    Remove checksum compute for now
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/migrations/files.ts
+++ cli/src/migrations/files.ts
@@ -1,9 +1,7 @@
-import { readFile, readdir, writeFile, mkdir, rm } from 'fs/promises';
-import path from 'path';
-import crypto from 'crypto';
-import { migrationFile, MigrationFile } from './schema.js';
 import { Schemas } from '@xata.io/client';
-import canonicalize from 'canonicalize';
+import { mkdir, readdir, readFile, rm, writeFile } from 'fs/promises';
+import path from 'path';
+import { migrationFile, MigrationFile } from './schema.js';
 
 const migrationsDir = path.join(process.cwd(), '.xata', 'migrations');
 const ledgerFile = path.join(migrationsDir, '.ledger');
@@ -59,22 +57,6 @@ export async function getLocalMigrationFiles() {
       throw new Error(`Failed to parse migration file ${filePath}: ${result.error}`);
     }
 
-    const checksum = await computeChecksum(result.data);
-    // TODO: Remove the existance check once backend backfills checksums
-    if (result.data.checksum && result.data.checksum !== checksum) {
-      console.warn(
-        `Checksum for migration ${result.data.id} does not match, please run 'xata pull -f' to overwrite local migrations'`
-      );
-      console.warn(`Expected ${result.data.checksum}, got ${checksum}`);
-    }
-
-    const fileChecksum = entry.split('_').slice(-1)[0];
-    if (!checksum.startsWith(fileChecksum)) {
-      console.warn(
-        `Checksum for migration ${result.data.id} does not match, please run 'xata pull -f' to overwrite local migrations'`
-      );
-    }
-
     migrations.push(result.data);
   }
 
@@ -85,18 +67,7 @@ export async function writeLocalMigrationFiles(files: MigrationFile[]) {
   const ledger = await getLedger();
 
   for (const file of files) {
-    const checksum = await computeChecksum(file);
-
-    // TODO: Remove the existance check once backend backfills checksums
-    if (file.checksum && file.checksum !== checksum) {
-      console.warn(
-        `Checksum for migration ${file.id} does not match, please run 'xata pull -f' to overwrite local migrations'`
-      );
-
-      console.warn(`Expected ${file.checksum}, got ${checksum}`);
-    }
-
-    const name = [file.id, checksum.slice(0, 8)].filter((item) => !!item).join('_');
+    const name = [file.id, file.checksum.slice(0, 8)].filter((item) => !!item).join('_');
     const filePath = path.join(migrationsDir, `${name}.json`);
     await writeFile(filePath, JSON.stringify(file, null, 2) + '\n', 'utf8');
 
@@ -108,19 +79,6 @@ export async function writeLocalMigrationFiles(files: MigrationFile[]) {
 
 export async function removeLocalMigrations() {
   await rm(migrationsDir, { recursive: true });
-}
-
-export async function computeChecksum(file: MigrationFile): Promise<string> {
-  const input = canonicalize({
-    id: file.id,
-    parentID: file.parent,
-    parentChecksum: file.checksum,
-    operations: file.operations
-  });
-
-  if (!input) throw new Error('Failed to canonicalize input to compute checksum');
-
-  return crypto.createHash('sha256').update(input).digest('hex');
 }
 
 export function commitToMigrationFile(logs: Schemas.Commit[]): MigrationFile[] {
