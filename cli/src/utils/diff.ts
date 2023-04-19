7c9a7bb7e9edf0bbb1b8a4b25288e1b8512a93c1
Alexis Rico <sferadev@gmail.com>
2023-04-19 13:53:57 +0100

    Update `diff` command
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/utils/diff.ts
+++ cli/src/utils/diff.ts
@@ -4,17 +4,19 @@ import chalk from 'chalk';
 export function buildMigrationDiff(ops: Schemas.MigrationOp[]): string {
   const lines = ops.map((op) => {
     if ('addTable' in op) {
-      return chalk`{green +} {bold ${op.addTable.table}}`;
+      return `${chalk.green('+')} ${chalk.bold(op.addTable.table)}`;
     } else if ('removeTable' in op) {
-      return chalk`{red -} {bold ${op.removeTable.table}}`;
+      return `${chalk.red('-')} ${chalk.bold(op.removeTable.table)}`;
     } else if ('renameTable' in op) {
-      return chalk`{yellow ~} {bold ${op.renameTable.oldName}} -> {bold ${op.renameTable.newName}}`;
+      return `${chalk.yellow('~')} ${chalk.bold(op.renameTable.oldName)} -> ${chalk.bold(op.renameTable.newName)}`;
     } else if ('addColumn' in op) {
-      return chalk`{green +} {bold ${op.addColumn.table}}.{bold ${op.addColumn.column.name}}`;
+      return `${chalk.green('+')} ${chalk.bold(op.addColumn.table)}.${chalk.bold(op.addColumn.column.name)}`;
     } else if ('removeColumn' in op) {
-      return chalk`{red -} {bold ${op.removeColumn.table}}.{bold ${op.removeColumn.column}}`;
+      return `${chalk.red('-')} ${chalk.bold(op.removeColumn.table)}.${chalk.bold(op.removeColumn.column)}`;
     } else if ('renameColumn' in op) {
-      return chalk`{yellow ~} {bold ${op.renameColumn.table}}.{bold ${op.renameColumn.oldName}} -> {bold ${op.renameColumn.newName}}`;
+      return `${chalk.yellow('~')} ${chalk.bold(op.renameColumn.table)}.${chalk.bold(
+        op.renameColumn.oldName
+      )} -> ${chalk.bold(op.renameColumn.newName)}`;
     } else {
       throw new Error(`Unknown migration op: ${JSON.stringify(op)}`);
     }
