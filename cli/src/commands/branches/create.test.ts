882d9a08be559348cf369aa290edc3f0c4792b5f
Alexis Rico <sferadev@gmail.com>
2023-04-19 05:54:21 +0200

    Remove `git` from create and list commands
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- cli/src/commands/branches/create.test.ts
+++ cli/src/commands/branches/create.test.ts
@@ -1,7 +1,6 @@
 import { Config } from '@oclif/core';
 import fetch from 'node-fetch';
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
-import * as git from '../../git.js';
 import { clearEnvVariables } from '../utils.test.js';
 import BranchesCreate from './create.js';
 
@@ -41,7 +40,7 @@ describe('branches create', () => {
     });
 
     const config = await Config.load();
-    const command = new BranchesCreate(['featureA', '--no-git'], config);
+    const command = new BranchesCreate(['featureA'], config);
     command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
 
     await expect(command.run()).rejects.toThrow('Something went wrong');
@@ -51,14 +50,14 @@ describe('branches create', () => {
     expect(fetchMock.mock.calls[0][1].method).toEqual('PUT');
   });
 
-  test.each([[false], [true]])('performs the creation with JSON enabled = %o and no git integration', async (json) => {
+  test.each([[false], [true]])('performs the creation with JSON enabled = %o', async (json) => {
     fetchMock.mockReturnValue({
       ok: true,
       json: async () => ({})
     });
 
     const config = await Config.load();
-    const command = new BranchesCreate(['featureA', '--no-git'], config);
+    const command = new BranchesCreate(['featureA'], config);
     command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
 
     expect(BranchesCreate.enableJsonFlag).toBe(true);
@@ -84,140 +83,4 @@ describe('branches create', () => {
       expect(log.mock.calls[0][0]).toEqual('✔ Branch featureA successfully created');
     }
   });
-
-  test('fails if git is not installed', async () => {
-    vi.spyOn(git, 'isGitInstalled').mockReturnValue(false);
-
-    const config = await Config.load();
-    const command = new BranchesCreate(['featureA'], config);
-    command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
-
-    await expect(command.run()).rejects.toThrow(
-      'Git cannot be found. Please install it or use the --no-git flag to disable integrating xata branches with git branches.'
-    );
-  });
-
-  test('fails if the working directory is not clean', async () => {
-    vi.spyOn(git, 'isGitInstalled').mockReturnValue(true);
-    vi.spyOn(git, 'isWorkingDirClean').mockReturnValue(false);
-
-    const config = await Config.load();
-    const command = new BranchesCreate(['featureA'], config);
-    command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
-
-    await expect(command.run()).rejects.toThrow(
-      'The working directory has uncommited changes. Please commit or stash them before creating a branch. Or use the --no-git flag to disable integrating xata branches with git branches.'
-    );
-  });
-
-  test('fails if the working directory is not a git repository', async () => {
-    vi.spyOn(git, 'isGitInstalled').mockReturnValue(true);
-    vi.spyOn(git, 'isWorkingDirClean').mockImplementation(() => {
-      throw new Error('fatal. not a git repository');
-    });
-
-    const config = await Config.load();
-    const command = new BranchesCreate(['featureA'], config);
-    command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
-
-    await expect(command.run()).rejects.toThrow(
-      'The working directory is not under git version control. Please initialize or clone a git repository or use the --no-git flag to disable integrating xata branches with git branches.'
-    );
-  });
-
-  test.each([[false], [true]])(
-    'creates a branch with JSON enabled = %o, git integration and no base branch',
-    async (json) => {
-      vi.spyOn(git, 'isGitInstalled').mockReturnValue(true);
-      vi.spyOn(git, 'isWorkingDirClean').mockReturnValue(true);
-      vi.spyOn(git, 'defaultGitBranch').mockReturnValue('def');
-      const createBranch = vi.spyOn(git, 'createBranch').mockReturnValue(undefined);
-
-      const config = await Config.load();
-      const command = new BranchesCreate(['featureA'], config);
-      command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
-
-      expect(BranchesCreate.enableJsonFlag).toBe(true);
-      vi.spyOn(command, 'jsonEnabled').mockReturnValue(json);
-
-      const log = vi.spyOn(command, 'log');
-
-      const result = await command.run();
-
-      if (json) {
-        expect(result).toMatchInlineSnapshot('{}');
-      } else {
-        expect(result).toBeUndefined();
-      }
-
-      expect(fetchMock).toHaveBeenCalledTimes(1);
-      expect(fetchMock.mock.calls[0][0]).toEqual('https://test-1234.eu-west-1.xata.sh/db/test:featureA');
-      expect(fetchMock.mock.calls[0][1].method).toEqual('PUT');
-
-      expect(createBranch).toHaveBeenCalledOnce();
-      expect(createBranch.mock.calls[0][0]).toEqual('featureA');
-      expect(createBranch.mock.calls[0][1]).toEqual('def');
-
-      expect(log).toHaveBeenCalledTimes(json ? 0 : 1);
-
-      if (!json) {
-        expect(log.mock.calls[0][0]).toEqual(
-          '✔ Branch featureA successfully created. A new git branch with the same name has been created and is your current branch.'
-        );
-      }
-    }
-  );
-
-  test.each([[false], [true]])(
-    'creates a branch with JSON enabled = %o, git integration and base branch',
-    async (json) => {
-      vi.spyOn(git, 'isGitInstalled').mockReturnValue(true);
-      vi.spyOn(git, 'isWorkingDirClean').mockReturnValue(true);
-      vi.spyOn(git, 'defaultGitBranch').mockReturnValue('def');
-      const createBranch = vi.spyOn(git, 'createBranch').mockReturnValue(undefined);
-      fetchMock.mockReturnValue({ ok: true, json: async () => ({ branch: 'base' }) });
-
-      const config = await Config.load();
-      const command = new BranchesCreate(['featureA', '--from', 'base'], config);
-      command.projectConfig = { databaseURL: 'https://test-1234.eu-west-1.xata.sh/db/test' };
-
-      expect(BranchesCreate.enableJsonFlag).toBe(true);
-      vi.spyOn(command, 'jsonEnabled').mockReturnValue(json);
-
-      const log = vi.spyOn(command, 'log');
-
-      const result = await command.run();
-
-      if (json) {
-        expect(result).toMatchInlineSnapshot(`
-          {
-            "branch": "base",
-          }
-        `);
-      } else {
-        expect(result).toBeUndefined();
-      }
-
-      expect(fetchMock).toHaveBeenCalledTimes(2);
-      expect(fetchMock.mock.calls[0][0]).toEqual(
-        'https://test-1234.eu-west-1.xata.sh/dbs/test/resolveBranch?gitBranch=base'
-      );
-      expect(fetchMock.mock.calls[0][1].method).toEqual('GET');
-      expect(fetchMock.mock.calls[1][0]).toEqual('https://test-1234.eu-west-1.xata.sh/db/test:featureA');
-      expect(fetchMock.mock.calls[1][1].method).toEqual('PUT');
-      expect(fetchMock.mock.calls[1][1].body).toMatchInlineSnapshot('"{\\"from\\":\\"base\\"}"');
-
-      expect(createBranch).toHaveBeenCalledOnce();
-      expect(createBranch.mock.calls[0][0]).toEqual('featureA');
-      expect(createBranch.mock.calls[0][1]).toEqual('base');
-
-      expect(log).toHaveBeenCalledTimes(json ? 0 : 1);
-
-      if (!json) {
-        expect(log.mock.calls[0][0]).toEqual(
-          '✔ Branch featureA successfully created. A new git branch with the same name has been created and is your current branch.'
-        );
-      }
-    }
-  );
 });
--- cli/src/commands/branches/create.ts
+++ cli/src/commands/branches/create.ts
@@ -1,6 +1,5 @@
 import { Args, Flags } from '@oclif/core';
 import { BaseCommand } from '../../base.js';
-import { createBranch, currentGitBranch, defaultGitBranch, isGitInstalled, isWorkingDirClean } from '../../git.js';
 
 export default class BranchesCreate extends BaseCommand<typeof BranchesCreate> {
   static description = 'Create a branch';
@@ -12,9 +11,6 @@ export default class BranchesCreate extends BaseCommand<typeof BranchesCreate> {
     ...this.databaseURLFlag,
     from: Flags.string({
       description: 'Branch name to branch off from'
-    }),
-    'no-git': Flags.boolean({
-      description: 'Disable git integration'
     })
   };
 
@@ -32,49 +28,13 @@ export default class BranchesCreate extends BaseCommand<typeof BranchesCreate> {
 
     const xata = await this.getXataClient();
 
-    const { 'no-git': noGit, from } = flags;
-    const useGit = !noGit;
-
-    if (useGit) {
-      if (!isGitInstalled()) {
-        this.error(
-          'Git cannot be found. Please install it or use the --no-git flag to disable integrating xata branches with git branches.'
-        );
-      }
-      try {
-        if (!isWorkingDirClean()) {
-          this.error(
-            'The working directory has uncommited changes. Please commit or stash them before creating a branch. Or use the --no-git flag to disable integrating xata branches with git branches.'
-          );
-        }
-
-        const currentBranch = currentGitBranch();
-        if (currentBranch !== branch) {
-          const { branch: gitBase } = from
-            ? await xata.api.branches.resolveBranch({ workspace, region, database, gitBranch: from })
-            : { branch: defaultGitBranch() };
-
-          createBranch(branch, gitBase);
-        }
-      } catch (err) {
-        if (err instanceof Error && err.message.includes('not a git repository')) {
-          this.error(
-            'The working directory is not under git version control. Please initialize or clone a git repository or use the --no-git flag to disable integrating xata branches with git branches.'
-          );
-        } else {
-          throw err;
-        }
-      }
-    }
+    const { from } = flags;
 
     const result = await xata.api.branches.createBranch({ workspace, region, database, branch, from });
 
     if (this.jsonEnabled()) return result;
 
-    let message = `Branch ${branch} successfully created`;
-    if (useGit) {
-      message = `${message}. A new git branch with the same name has been created and is your current branch.`;
-    }
+    const message = `Branch ${branch} successfully created`;
 
     this.success(message);
   }
