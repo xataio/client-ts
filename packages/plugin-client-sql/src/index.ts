ee585f19e7b3485791a09ced58e58462c1a4f576
Alexis Rico <sferadev@gmail.com>
2023-06-28 11:00:21 +0200

    Remove kysely builder

--- packages/plugin-client-sql/src/index.ts
+++ packages/plugin-client-sql/src/index.ts
@@ -1,29 +1,7 @@
-import type { XataRecord } from '@xata.io/client';
-import { EditableData, XataPlugin, XataPluginOptions } from '@xata.io/client';
-import { Generated, Kysely, PostgresDialect, sql } from 'kysely';
-import { Pool } from 'pg';
+import { XataPlugin, XataPluginOptions } from '@xata.io/client';
 
-type XataToKysely<T> = {
-  [K in keyof T]: T[K] extends XataRecord ? EditableData<T[K]> & { id: Generated<string> } : T[K];
-};
-
-export class XataSQLPlugin<Database> extends XataPlugin {
+export class XataSQLPlugin extends XataPlugin {
   build(_options: XataPluginOptions) {
-    const db = new Kysely<XataToKysely<Database>>({
-      dialect: new PostgresDialect({
-        pool: new Pool({
-          host: '127.0.0.1',
-          port: 6543,
-          database: 'demo:main',
-          user: '',
-          password: ''
-        })
-      })
-    });
-
-    return {
-      builder: db,
-      query: (query: string) => sql`${query}`.execute(db)
-    };
+    return;
   }
 }
