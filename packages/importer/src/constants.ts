a66ef617233e210cde684bffb531fffec4ca48f1
Richard Gill <richard@rgill.co.uk>
2023-07-05 16:16:09 +0100

    added comment

--- packages/importer/src/constants.ts
+++ packages/importer/src/constants.ts
@@ -1,3 +1,4 @@
 import { z } from 'zod';
 
+// We don't yet support importing all Xata column types
 export const importColumnTypes = z.enum(['bool', 'int', 'float', 'string', 'text', 'email', 'datetime']);
