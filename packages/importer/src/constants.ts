97d4082eab8463a45f2ea729fb623faa37627830
Richard Gill <richardgill@users.noreply.github.com>
2023-07-12 12:42:24 +0100

    @xata.io/importer v2 (#1051)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>
    Co-authored-by: Alexis Rico <sferadev@gmail.com>
    Co-authored-by: Xata Bot <97746683+xata-bot@users.noreply.github.com>
    Co-authored-by: SferaDev <SferaDev@users.noreply.github.com>

---
+++ packages/importer/src/constants.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod';
+
+// We don't yet support importing all Xata column types
+export const importColumnTypes = z.enum(['bool', 'int', 'float', 'string', 'text', 'email', 'datetime', 'link']);
---
+++ packages/importer/src/parsers/csvParser.ts
@@ -0,0 +1,84 @@
+import pick from 'lodash.pick';
+import CSV, { ParseConfig } from 'papaparse';
+import { ParseCsvOptions, ParseMeta, ParseResults } from '../types';
+import { parseJson } from './jsonParser';
+
+export const DEFAULT_CSV_DELIMITERS_TO_GUESS = [',', '\t', '|', ';', '\x1E', '\x1F'];
+
+// https://github.com/sindresorhus/strip-bom/blob/main/index.js
+const stripBom = (string: string) => {
+  if (typeof string !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof string}`);
+  }
+  // Catches EFBBBF (UTF-8 BOM) because the buffer-to-string
+  // conversion translates it to FEFF (UTF-16 BOM).
+  if (string.charCodeAt(0) === 0xfeff) {
+    return string.slice(1);
+  }
+  return string;
+};
+
+export const metaToParseMeta = (meta: Papa.ParseMeta): Omit<ParseMeta, 'estimatedProgress'> => ({
+  delimiter: meta.delimiter,
+  linebreak: meta.linebreak,
+  fields: meta.fields
+});
+
+export const parseCsvOptionsToPapaOptions = (options: ParseCsvOptions): ParseConfig => {
+  const {
+    limit,
+    delimiter,
+    header = true,
+    skipEmptyLines = true,
+    delimitersToGuess = DEFAULT_CSV_DELIMITERS_TO_GUESS,
+    newline,
+    quoteChar = '"',
+    escapeChar = '"',
+    commentPrefix
+  } = options;
+  return {
+    header,
+    skipEmptyLines,
+    preview: limit,
+    delimiter,
+    delimitersToGuess,
+    newline,
+    quoteChar,
+    escapeChar,
+    comments: commentPrefix,
+    transformHeader(header) {
+      return stripBom(header);
+    }
+  };
+};
+
+const dataForColumns = (data: unknown[], columns: ParseCsvOptions['columns']) => {
+  if (!columns) {
+    return data;
+  }
+  return data.map((d) =>
+    pick(
+      d,
+      columns.map((col) => col.name)
+    )
+  );
+};
+
+export const papaResultToJson = (
+  { data, errors }: CSV.ParseResult<unknown>,
+  options: ParseCsvOptions
+): ParseResults => {
+  const parseWarnings = errors.map((error) => error.message);
+
+  const jsonResults = parseJson({
+    ...options,
+    data: dataForColumns(data, options.columns)
+  });
+
+  return jsonResults.success
+    ? {
+        ...jsonResults,
+        warnings: [...parseWarnings, ...jsonResults.warnings]
+      }
+    : jsonResults;
+};
