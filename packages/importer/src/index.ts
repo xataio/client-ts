8d7788bbfc45712e56572ba16e221230a58ef422
Richard Gill <richard@rgill.co.uk>
2023-07-14 06:23:42 +0100

    export more internals in @xata.io/importer

--- packages/importer/src/index.ts
+++ packages/importer/src/index.ts
@@ -1,6 +1,15 @@
 export { guessColumnTypes } from './columns';
 export { DEFAULT_CSV_DELIMITERS_TO_GUESS } from './parsers/csvParser';
+export { parseCsvStream, parseCsvStreamBatches } from './csvStreamParser';
 export * from './plugin';
-export type { CsvResults, ImportError, ImportBatchOptions, ParseMeta, ParseResults } from './types';
+export type {
+  CsvResults,
+  ImportError,
+  ImportBatchOptions,
+  ParseMeta,
+  ParseResults,
+  ParseCsvStreamOptions,
+  ParseCsvStreamBatchesOptions
+} from './types';
 export { generateRandomData } from './random-data';
 export { importColumnTypes } from './constants';
