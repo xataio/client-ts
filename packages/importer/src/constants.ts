eafb153a036043682fff73c099a3dccf3e473b10
Richard Gill <richard@rgill.co.uk>
2023-07-24 13:21:35 +0100

    multiple support in CLI

--- packages/importer/src/constants.ts
+++ packages/importer/src/constants.ts
@@ -1,4 +1,14 @@
 import { z } from 'zod';
 
 // We don't yet support importing all Xata column types
-export const importColumnTypes = z.enum(['bool', 'int', 'float', 'string', 'text', 'email', 'datetime', 'link']);
+export const importColumnTypes = z.enum([
+  'bool',
+  'int',
+  'float',
+  'string',
+  'text',
+  'email',
+  'datetime',
+  'link',
+  'multiple'
+]);
