8fbf9132ae728f36c736f99271c10b07ea597c3c
Alexis Rico <sferadev@gmail.com>
2023-08-03 08:48:27 +0200

    Export file helpers (#1111)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -14,5 +14,7 @@ export * from './schema';
 export * from './search';
 export * from './sql';
 export * from './serializer';
+export * from './files';
+export { transformImage } from './files/transformations';
 export { getAPIKey, getBranch, getDatabaseURL, getPreviewBranch, buildPreviewBranchName } from './util/environment';
 export * from './workers';
