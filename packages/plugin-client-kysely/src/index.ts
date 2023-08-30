d6886d67130999b38c5e2bb124ca9ff90fc89524
Alexis Rico <sferadev@gmail.com>
2023-08-30 12:08:59 +0200

    [SQL] Update kysely exports (#1142)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/plugin-client-kysely/src/index.ts
+++ packages/plugin-client-kysely/src/index.ts
@@ -2,18 +2,20 @@ import { EditableData, SQLPlugin, XataPlugin, XataPluginOptions, XataRecord } fr
 import { Kysely } from 'kysely';
 import { XataDialect } from './driver';
 
-export type KyselyPluginResult<Schemas extends Record<string, XataRecord>> = Kysely<TransformSchema<Schemas>>;
+export type KyselyPluginResult<Schemas extends Record<string, XataRecord>> = Kysely<Model<Schemas>>;
 
 export class KyselyPlugin<Schemas extends Record<string, XataRecord>> extends XataPlugin {
   build(pluginOptions: XataPluginOptions): KyselyPluginResult<Schemas> {
     const xata = { sql: new SQLPlugin().build(pluginOptions) };
 
-    return new Kysely<TransformSchema<Schemas>>({
+    return new Kysely<Model<Schemas>>({
       dialect: new XataDialect({ xata })
     });
   }
 }
 
-type TransformSchema<Schemas extends Record<string, XataRecord>> = {
+export type Model<Schemas extends Record<string, XataRecord>> = {
   [Model in keyof Schemas]: EditableData<Schemas[Model]>;
 };
+
+export * from './driver';
