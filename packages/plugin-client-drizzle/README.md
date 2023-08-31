7e0139f150faac1a3d37b1ec76c2649c6ce09a2c
Alexis Rico <sferadev@gmail.com>
2023-08-31 08:26:34 +0200

    [SQL] Add driver for drizzle
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

---
+++ packages/plugin-client-drizzle/README.md
@@ -0,0 +1,20 @@
+# @xata.io/drizzle
+
+A [Drizzle](https://github.com/drizzle-team/drizzle-orm) driver for [Xata](https://xata.io), using the [Xata serverless driver for TypeScript](https://github.com/xataio/client-ts).
+
+The goal of this package is polish the implementation and contribute it back to the official Drizzle repository.
+
+## Installation
+
+You should install both `drizzle-orm` and `@xata.io/drizzle` with `@xata.io/client`. You can install them with your favorite package manager:
+
+```bash
+# with pnpm
+pnpm add drizzle-orm @xata.io/drizzle @xata.io/client
+
+# with yarn
+yarn add drizzle-orm @xata.io/drizzle @xata.io/client
+
+# with npm
+npm install drizzle-orm @xata.io/drizzle @xata.io/client
+```
---
+++ packages/plugin-client-drizzle/src/session.ts
@@ -0,0 +1,147 @@
+import {
+  Assume,
+  Logger,
+  NoopLogger,
+  Query,
+  RelationalSchemaConfig,
+  TablesRelationalConfig,
+  entityKind,
+  fillPlaceholders
+} from 'drizzle-orm';
+import {
+  PgDialect,
+  PgSession,
+  PgTransactionConfig,
+  PreparedQuery,
+  PreparedQueryConfig,
+  QueryResultHKT,
+  SelectedFieldsOrdered
+} from 'drizzle-orm/pg-core';
+import { XataClient } from './driver';
+import { mapResultRow } from './utils';
+
+type QueryResult<T = Record<string, unknown>> = {
+  records: T[];
+  warning?: string;
+};
+
+export class XataPreparedQuery<T extends PreparedQueryConfig> extends PreparedQuery<T> {
+  static readonly [entityKind]: string = 'VercelPgPreparedQuery';
+
+  private query: {
+    name: string | undefined;
+    statement: string;
+  };
+
+  constructor(
+    private client: XataClient,
+    queryString: string,
+    private params: unknown[],
+    private logger: Logger,
+    private fields: SelectedFieldsOrdered | undefined,
+    name: string | undefined,
+    private customResultMapper?: (rows: unknown[][]) => T['execute']
+  ) {
+    super();
+
+    this.query = {
+      name,
+      statement: queryString
+    };
+  }
+
+  async execute(placeholderValues: Record<string, unknown> | undefined = {}): Promise<T['execute']> {
+    const params = fillPlaceholders(this.params, placeholderValues);
+
+    this.logger.logQuery(this.query.statement, params);
+
+    const { records } = await this.client.sql<Record<string, unknown>>({
+      statement: this.query.statement,
+      params
+    });
+
+    const rows = records.map((record) => this.fields!.map((field) => record[field.path.join('.')]));
+
+    if (this.customResultMapper) {
+      return this.customResultMapper(rows);
+    }
+
+    return rows.map((row) => mapResultRow<T['execute']>(this.fields!, row, undefined));
+  }
+
+  async all(placeholderValues: Record<string, unknown> | undefined = {}): Promise<T['all']> {
+    const params = fillPlaceholders(this.params, placeholderValues);
+    this.logger.logQuery(this.query.statement, params);
+
+    const { records } = await this.client.sql({
+      statement: this.query.statement,
+      params
+    });
+
+    return records;
+  }
+
+  async values(placeholderValues: Record<string, unknown> | undefined = {}): Promise<T['values']> {
+    const params = fillPlaceholders(this.params, placeholderValues);
+    this.logger.logQuery(this.query.statement, params);
+
+    const { records } = await this.client.sql({
+      statement: this.query.statement,
+      params
+    });
+
+    return records;
+  }
+}
+
+export interface XataSessionOptions {
+  logger?: Logger;
+}
+
+export class XataSession<
+  TFullSchema extends Record<string, unknown>,
+  TSchema extends TablesRelationalConfig
+> extends PgSession<XataQueryResultHKT, TFullSchema, TSchema> {
+  static readonly [entityKind]: string = 'VercelPgSession';
+
+  private logger: Logger;
+
+  constructor(
+    private client: XataClient,
+    dialect: PgDialect,
+    private schema: RelationalSchemaConfig<TSchema> | undefined,
+    private options: XataSessionOptions = {}
+  ) {
+    super(dialect);
+    this.logger = options.logger ?? new NoopLogger();
+  }
+
+  prepareQuery<T extends PreparedQueryConfig = PreparedQueryConfig>(
+    query: Query,
+    fields: SelectedFieldsOrdered | undefined,
+    name: string | undefined,
+    customResultMapper?: (rows: unknown[][]) => T['execute']
+  ): PreparedQuery<T> {
+    return new XataPreparedQuery(this.client, query.sql, query.params, this.logger, fields, name, customResultMapper);
+  }
+
+  async query(query: string, params: unknown[]): Promise<QueryResult> {
+    this.logger.logQuery(query, params);
+    return await this.client.sql({ statement: query, params });
+  }
+
+  async queryObjects<T extends Record<string, unknown>>(query: string, params: unknown[]): Promise<QueryResult<T>> {
+    return this.client.sql({ statement: query, params });
+  }
+
+  override async transaction<T>(
+    _transaction: (tx: any) => Promise<T>,
+    _config?: PgTransactionConfig | undefined
+  ): Promise<T> {
+    throw new Error('Transactions are not supported');
+  }
+}
+
+export interface XataQueryResultHKT extends QueryResultHKT {
+  type: QueryResult<Assume<this['row'], Record<string, unknown>>[]>;
+}
