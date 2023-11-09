32d5d23e4df276f1214024a0a30d29e525e99eb7
Emily <emily.morgan@code.berlin>
2023-11-09 11:53:26 +0100

    fix

--- scripts/test-canary.ts
+++ scripts/test-canary.ts
@@ -6,49 +6,45 @@ async function main() {
   if (!process.env.XATA_BRANCH) throw new Error('XATA_BRANCH is not set');
 
   console.log(`Running canary test for ${process.env.CANARY_VERSION}`);
-  console.log(`Running canary test against ${process.env.XATA_DATABASE_URL}:${process.env.XATA_BRANCH}`);
 
   const cli = `@xata.io/cli@${process.env.CANARY_VERSION}`;
 
-  const download = new Promise((resolve, reject) => {
-    exec(`npx -y ${cli}`, async (error) => {
-      if (error) {
-        console.log('error is:', error);
-        //or
-        reject(error);
-      }
+  const download = new Promise((resolve) => {
+    const command = exec(`npx -y ${cli}`);
+    command.stdout?.on('data', (data) => {
+      console.log(data);
       resolve('done');
+    });
+    command.stderr?.on('data', (data) => {
+      throw data;
     });
   });
   await download;
 
-  const init = new Promise((resolve, reject) => {
-    exec(`npx ${cli} init --db ${process.env.XATA_DATABASE_URL} --force`, async (error) => {
-      if (error) {
-        console.log('error is:', error);
-        //or
-        reject(error);
-      }
-      console.log('successfully initialized');
+  const init = new Promise((resolve) => {
+    const command = exec(`npx ${cli} init --db ${process.env.XATA_DATABASE_URL} --force`);
+    command.stdout?.on('data', (data) => {
+      console.log(data);
       resolve('done');
     });
+    command.stderr?.on('data', (data) => {
+      throw data;
+    });
   });
-
   await init;
 
-  const schemaPull = new Promise((resolve, reject) => {
-    exec(`npx ${cli} pull -badflag ${process.env.XATA_BRANCH} --db ${process.env.XATA_DATABASE_URL}`, async (error) => {
-      if (error) {
-        console.log('error is:', error);
-        //or
-        reject(error);
-      }
-      console.log('successfully pulled migrations');
+  const schemaPull = new Promise((resolve) => {
+    const command = exec(`npx ${cli} pull ${process.env.XATA_BRANCH} --db ${process.env.XATA_DATABASE_URL}`);
+    command.stdout?.on('data', (data) => {
+      console.log(data);
       resolve('done');
     });
+    command.stderr?.on('data', (data) => {
+      throw data;
+    });
   });
-
   await schemaPull;
+
   console.log(`Completed successfully`);
 }
 
