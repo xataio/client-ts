6cbf701e4884b6f65592d197cd483848610d56f5
Emily <emily.morgan@code.berlin>
2023-12-18 13:50:45 +0100

    clean

--- test/unit/apiProxyTypes.test.ts
+++ test/unit/apiProxyTypes.test.ts
@@ -11,7 +11,7 @@ beforeAll(async () => {
 });
 
 describe('API Proxy types', () => {
-  test('returns functions for all defined namespace methods', async () => {
+  test('returns functions for all defined namespace operations', () => {
     for (const namespace of Object.keys(operationsByTag)) {
       const operationsInNamespace = operationsByTag[namespace as keyof typeof operationsByTag];
       for (const operation of Object.keys(operationsInNamespace)) {
@@ -19,11 +19,11 @@ describe('API Proxy types', () => {
       }
     }
   });
-  test('returns undefined for undefined namespaces', async () => {
+  test('returns undefined for undefined namespaces', () => {
     // @ts-ignore
     expect(xata.undefinedNamespace).toBeUndefined();
   });
-  test('returns undefined for undefined namespace operations', async () => {
+  test('returns undefined for undefined namespace operations', () => {
     // @ts-ignore
     expect(xata.authentication.undefinedMethod).toBeUndefined();
   });
