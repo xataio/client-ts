b085f59a77d5a96eacf9db1d9aa9207af9bee8d3
Alexis Rico <sferadev@gmail.com>
2024-01-02 19:16:13 +0100

    Update packages/pgroll/src/index.ts

--- packages/pgroll/src/index.ts
+++ packages/pgroll/src/index.ts
@@ -2,7 +2,7 @@ import { schema } from './schema';
 
 export * from './types';
 
-export const PGROLL_JSON_SCHEMA_URL = 'https://raw.githubusercontent.com/xataio/pgroll/fix-schema/schema.json';
+export const PGROLL_JSON_SCHEMA_URL = 'https://raw.githubusercontent.com/xataio/pgroll/main/schema.json';
 
 // In the future, we can use this function to generate the JSON schema tailored to the user's data model.
 export function generateJSONSchema() {
