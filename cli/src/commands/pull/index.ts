538673fc0ef83cfe7bd4c9a76221a76e10e00990
Alexis Rico <sferadev@gmail.com>
2024-01-09 08:59:44 +0100

    Make XataApiClient to use ES Proxies (#1287)

--- cli/src/commands/pull/index.ts
+++ cli/src/commands/pull/index.ts
@@ -43,13 +43,12 @@ export default class Pull extends BaseCommand<typeof Pull> {
     );
 
     const { logs } = await xata.api.migrations.getBranchSchemaHistory({
-      workspace,
-      region,
-      database,
-      branch,
-      // TODO: Fix pagination in the API to start from last known migration and not from the beginning
-      // Also paginate until we get all migrations
-      page: { size: 200 }
+      pathParams: { workspace, region, dbBranchName: `${database}:${branch}` },
+      body: {
+        // TODO: Fix pagination in the API to start from last known migration and not from the beginning
+        // Also paginate until we get all migrations
+        page: { size: 200 }
+      }
     });
 
     if (flags.force) {
