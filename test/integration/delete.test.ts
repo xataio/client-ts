455368460950ce239e60a42f37be9e849ea01ae1
Alexis Rico <sferadev@gmail.com>
2024-02-28 16:54:14 +0100

    Rename internal columns breaking change (#1370)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- test/integration/delete.test.ts
+++ test/integration/delete.test.ts
@@ -30,13 +30,13 @@ describe('record deletion', () => {
   test('delete single team with id', async () => {
     const team = await xata.db.teams.create({ name: 'Team ships' });
 
-    await xata.db.teams.delete(team.id);
+    await xata.db.teams.delete(team.xata_id);
 
     const copy = await team.read();
 
     expect(copy).toBeNull();
 
-    const apiTeam = await xata.db.teams.filter({ id: team.id }).getFirst();
+    const apiTeam = await xata.db.teams.filter({ xata_id: team.xata_id }).getFirst();
 
     expect(apiTeam).toBeNull();
   });
@@ -44,17 +44,17 @@ describe('record deletion', () => {
   test('delete multiple teams with id list', async () => {
     const teams = await xata.db.teams.create([{ name: 'Team cars' }, { name: 'Team planes' }]);
 
-    const result = await xata.db.teams.delete(teams.map((team) => team.id));
+    const result = await xata.db.teams.delete(teams.map((team) => team.xata_id));
 
     expect(result.length).toBe(2);
-    expect(result[0]?.id).toBe(teams[0].id);
-    expect(result[1]?.id).toBe(teams[1].id);
+    expect(result[0]?.xata_id).toBe(teams[0].xata_id);
+    expect(result[1]?.xata_id).toBe(teams[1].xata_id);
     expect(result[0]?.read).toBeDefined();
     expect(result[1]?.read).toBeDefined();
     expect(result[0]?.name).toBe('Team cars');
     expect(result[1]?.name).toBe('Team planes');
 
-    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ id: t.id })) }).getAll();
+    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ xata_id: t.xata_id })) }).getAll();
 
     expect(apiTeams).toHaveLength(0);
   });
@@ -68,7 +68,7 @@ describe('record deletion', () => {
 
     expect(copy).toBeNull();
 
-    const apiTeam = await xata.db.teams.filter({ id: team.id }).getFirst();
+    const apiTeam = await xata.db.teams.filter({ xata_id: team.xata_id }).getFirst();
 
     expect(apiTeam).toBeNull();
   });
@@ -78,7 +78,7 @@ describe('record deletion', () => {
 
     await xata.db.teams.delete(teams);
 
-    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ id: t.id })) }).getAll();
+    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ xata_id: t.xata_id })) }).getAll();
 
     expect(apiTeams).toHaveLength(0);
   });
@@ -86,18 +86,18 @@ describe('record deletion', () => {
   test('delete multiple teams with invalid', async () => {
     const teams = await xata.db.teams.create([{ name: 'Team cars' }, { name: 'Team planes' }]);
 
-    const result = await xata.db.teams.delete([...teams, { id: 'invalid' }]);
+    const result = await xata.db.teams.delete([...teams, { xata_id: 'invalid' }]);
 
     expect(result.length).toBe(3);
-    expect(result[0]?.id).toBe(teams[0].id);
-    expect(result[1]?.id).toBe(teams[1].id);
+    expect(result[0]?.xata_id).toBe(teams[0].xata_id);
+    expect(result[1]?.xata_id).toBe(teams[1].xata_id);
     expect(result[0]?.read).toBeDefined();
     expect(result[1]?.read).toBeDefined();
     expect(result[0]?.name).toBe('Team cars');
     expect(result[1]?.name).toBe('Team planes');
     expect(result[2]).toBeNull();
 
-    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ id: t.id })) }).getAll();
+    const apiTeams = await xata.db.teams.filter({ $any: teams.map((t) => ({ xata_id: t.xata_id })) }).getAll();
     expect(apiTeams).toHaveLength(0);
   });
 
@@ -110,7 +110,7 @@ describe('record deletion', () => {
 
     expect(copy).toBeNull();
 
-    const apiTeam = await xata.db.teams.filter({ id: team.id }).getFirst();
+    const apiTeam = await xata.db.teams.filter({ xata_id: team.xata_id }).getFirst();
 
     expect(apiTeam).toBeNull();
   });
@@ -119,14 +119,14 @@ describe('record deletion', () => {
     const valid = await xata.db.teams.create({ name: 'Team ships' });
 
     const team1 = await xata.db.teams.delete('invalid');
-    const team2 = await xata.db.teams.delete({ id: 'invalid', name: 'Team boats' });
-    const team3 = await xata.db.teams.delete([{ id: 'invalid', name: 'Team boats' }, valid]);
+    const team2 = await xata.db.teams.delete({ xata_id: 'invalid', name: 'Team boats' });
+    const team3 = await xata.db.teams.delete([{ xata_id: 'invalid', name: 'Team boats' }, valid]);
 
     expect(team1).toBeNull();
     expect(team2).toBeNull();
     expect(team3[0]).toBeNull();
     expect(team3[1]).toBeDefined();
-    expect(team3[1]?.id).toBe(valid.id);
+    expect(team3[1]?.xata_id).toBe(valid.xata_id);
     expect(team3[1]?.name).toBe('Team ships');
   });
 
@@ -134,7 +134,7 @@ describe('record deletion', () => {
     const team = await xata.db.teams.create({ name: 'Team ships' });
 
     const result = await xata.db.teams.delete(team);
-    expect(result?.id).toBe(team.id);
+    expect(result?.xata_id).toBe(team.xata_id);
 
     const result2 = await xata.db.teams.delete(team);
     expect(result2).toBeNull();
