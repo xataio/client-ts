074570564b98b0d9cf3114e887632d0fbcce4475
Alexis Rico <sferadev@gmail.com>
2024-03-05 17:13:42 +0100

    Wait for 2 seconds after search has replicated
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- test/integration/smoke.test.ts
+++ test/integration/smoke.test.ts
@@ -187,16 +187,10 @@ async function waitForSearchIndexing(api: XataApiClient, workspace: string, data
       aggs: { total: { count: '*' } }
     });
 
-    const { totalCount } = await api.searchAndFilter.searchTable({
-      workspace,
-      database,
-      region,
-      branch: 'branch',
-      table: 'table',
-      query: 'example'
-    });
-
-    if (aggs?.total === 1 && totalCount === 1) return;
+    if (aggs?.total === 1) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return;
+    }
   } catch (error) {
     // do nothing
   }
