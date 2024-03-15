fd452b680bdaa3c72eba8bb2d6882556a9ffa0bf
Emily <emily.morgan@code.berlin>
2024-03-15 11:15:23 +0100

    feat: use pgType for random data

--- packages/importer/src/random-data.ts
+++ packages/importer/src/random-data.ts
@@ -1,20 +1,22 @@
 import { fakerEN as faker } from '@faker-js/faker';
 import { Schemas } from '@xata.io/client';
+import { z } from 'zod';
 
-export function generateRandomData(table: Schemas.Table, size: number) {
+export function generateRandomData(table: Schemas.Table, size: number, pgRollEnabled?: boolean) {
   const records: Record<string, unknown>[] = [];
 
   for (let index = 0; index < size; index++) {
-    records.push(randomRecord(table.columns));
+    records.push(randomRecord(table.columns, pgRollEnabled));
   }
 
   return records;
 }
 
-function randomRecord(columns: Schemas.Column[]) {
+function randomRecord(columns: Schemas.Column[], pgRollEnabled?: boolean) {
   const record: Record<string, unknown> = {};
   for (const column of columns) {
-    record[column.name] = randomData(column);
+    // TODO column should contain pgType field from schema
+    record[column.name] = pgRollEnabled ? randomDataPgroll(column as any) : randomData(column);
   }
   return record;
 }
@@ -40,6 +42,39 @@ function randomData(column: Schemas.Column) {
     default:
       return undefined;
   }
+}
+
+function randomDataPgroll(column: Schemas.Column & { comment?: string; pgType: string }) {
+  switch (column.pgType) {
+    case 'boolean':
+    case 'bool':
+      return rand(0, 1) === 1;
+    case 'bigint':
+    case 'int8':
+    case 'integer':
+    case 'int':
+    case 'int4':
+    case 'smallint':
+      return rand(1, 100);
+    case 'double precision':
+    case 'float8':
+    case 'real':
+      return rand(1, 10000) / rand(1, 100);
+    case 'text':
+    case 'varchar':
+    case 'character varying':
+      return narrowStringType(column.comment) === 'email'
+        ? faker.internet.email({ provider: 'acme.pets' })
+        : faker.word.words(1);
+    case 'timestamptz':
+      return faker.date.recent({ days: rand(1, 10) });
+    case 'text[]':
+      return faker.word.words(rand(1, 3)).split(' ');
+  }
+  if (column.pgType.startsWith('character(') || column.pgType.startsWith('varchar(')) return faker.word.words(1);
+  if (column.pgType.startsWith('numeric(')) return rand(1, 10000) / rand(1, 100);
+
+  return undefined;
 }
 
 function rand(min: number, max: number) {
@@ -69,3 +104,15 @@ function randomString(columnName: string) {
   if (gen) return gen();
   return faker.word.words(2);
 }
+
+export const xataStringColumns = ['email', 'text', 'string'] as const;
+
+const XataStringColumn = z.object({
+  ['xata.type']: z.enum(xataStringColumns)
+});
+
+const narrowStringType = (comment?: string): Schemas.Column['type'] => {
+  if (!comment) return 'text';
+  const result = XataStringColumn.safeParse(JSON.parse(comment));
+  return result.success ? result.data['xata.type'] : 'text';
+};
