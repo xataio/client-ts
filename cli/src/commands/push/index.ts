3334baa3221e35b75a50b9d7ddff691ce3ad5489
Andrew Farries <andrew.farries@xata.io>
2024-04-09 14:54:28 +0100

    Wait for completion on `pgroll` migration push (#1434)

--- cli/src/commands/push/index.ts
+++ cli/src/commands/push/index.ts
@@ -1,5 +1,6 @@
 import { Args, Flags } from '@oclif/core';
 import { Schemas } from '@xata.io/client';
+import { PgRollMigrationDefinition } from '@xata.io/pgroll';
 import { BaseCommand } from '../../base.js';
 import {
   LocalMigrationFile,
@@ -11,10 +12,10 @@ import {
   allMigrationsPgRollFormat,
   getBranchDetailsWithPgRoll,
   isBranchPgRollEnabled,
-  isMigrationPgRollFormat
+  isMigrationPgRollFormat,
+  waitForMigrationToFinish
 } from '../../migrations/pgroll.js';
 import { MigrationFilePgroll } from '../../migrations/schema.js';
-import { PgRollMigrationDefinition } from '@xata.io/pgroll';
 
 export default class Push extends BaseCommand<typeof Push> {
   static description = 'Push local changes to a remote Xata branch';
@@ -107,14 +108,15 @@ export default class Push extends BaseCommand<typeof Push> {
         .flatMap((migration) => PgRollMigrationDefinition.parse(migration));
       for (const migration of migrationsToPush) {
         try {
-          await xata.api.branches.applyMigration({
+          const { jobID } = await xata.api.branches.applyMigration({
             workspace,
             region,
             database,
             branch,
-            // @ts-expect-error Backend API spec doesn't know all pgroll migrations yet
-            migration
+            migration: migration as any
           });
+
+          await waitForMigrationToFinish(xata.api, workspace, region, database, branch, jobID);
         } catch (e) {
           this.log(`Failed to push ${migration} with ${e}. Stopping.`);
           this.exit(1);
