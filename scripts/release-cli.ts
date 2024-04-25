8d2de3c5185f50cc4dcac7ef8b3b7bb35422acdf
Emily <emily.morgan@code.berlin>
2024-04-25 11:30:50 +0200

    try and bundle on ubuntu only

--- scripts/release-cli.ts
+++ scripts/release-cli.ts
@@ -21,24 +21,23 @@ const base = {
   }
 };
 
-const matrixToOclif = (os: string) => {
-  switch (os) {
-    case 'windows-latest':
-      return 'win';
-    case 'macos-latest':
-      return 'macos';
-    case 'ubuntu-latest':
-      return 'deb';
-    default:
-      throw new Error('Unsupported OS');
-  }
-};
+// const matrixToOclif = (os: string) => {
+//   switch (os) {
+//     case 'windows-latest':
+//       return 'win';
+//     case 'macos-latest':
+//       return 'macos';
+//     case 'ubuntu-latest':
+//       return 'deb';
+//     default:
+//       throw new Error('Unsupported OS');
+//   }
+// };
 
 async function main() {
-  if (!process.env.MATRIX_OS) throw new Error('MATRIX_OS is not set');
   if (!process.env.GITHUB_TOKEN) throw new Error('GITHUB_TOKEN is not set');
 
-  const operatingSystem = matrixToOclif(process.env.MATRIX_OS);
+  const operatingSystems = ['deb', 'win', 'macos'];
 
   const { manifest, fileName } = await readProjectManifest(PATH_TO_CLI);
   const {
@@ -75,11 +74,34 @@ async function main() {
   execFile('rm', ['-rf', `${PATH_TO_CLI}/npm-shrinkwrap.json`]);
   execFile('touch', [`${PATH_TO_CLI}/npm-shrinkwrap.json`]);
 
-  const pack = await exec(`pnpm oclif pack ${operatingSystem}`);
-  if (pack.stderr) {
-    throw new Error(`Failed to pack: ${pack.stderr}`);
+  for (const operatingSystem of operatingSystems) {
+    const build = await exec(`pnpm oclif build ${operatingSystem}`);
+    if (build.stderr) {
+      throw new Error(`Failed to build: ${build.stderr}`);
+    }
+    console.log('Successfully built CLI', build.stdout);
+
+    // windows installer is saved in "win32" folder in cli/dist
+    const pathToAsset = `${PATH_TO_CLI}/dist/${operatingSystem === 'win' ? 'win32' : operatingSystem}`;
+
+    const files = fs.readdirSync(pathToAsset);
+
+    for (const file of files) {
+      console.log('file in directory', file);
+      const data = fs.readFileSync(pathToAsset + `/${file}`);
+      console.log('data...', data);
+      // TODO debian has redundant packages. Only upload .deb files
+      // const upload = await octokit.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
+      //   ...base,
+      //   name: file,
+      //   label: file,
+      //   release_id: release.data.id,
+      //   data: data,
+      //   baseUrl: 'https://uploads.github.com'
+      // });
+      // console.log('Finished uploading asset', upload.status);
+    }
   }
-  console.log('Successfully packed CLI', pack.stdout);
 
   // const octokit = new Octokit({
   //   auth: process.env.GITHUB_TOKEN
@@ -93,44 +115,6 @@ async function main() {
   // });
 
   // if (!release.data) throw new Error('Release not found');
-
-  // windows installer is saved in "win32" folder in cli/dist
-  const pathToAsset = `${PATH_TO_CLI}/dist/${operatingSystem}`;
-  const pathToAssetWindows = operatingSystem === 'deb' ? `${PATH_TO_CLI}/dist/win32` : undefined;
-
-  const files = fs.readdirSync(pathToAsset);
-
-  for (const file of files) {
-    console.log('file in directory', file);
-    const data = fs.readFileSync(pathToAsset + `/${file}`);
-    console.log('data...', data);
-    // TODO debian has redundant packages. Only upload .deb files
-    // const upload = await octokit.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
-    //   ...base,
-    //   name: file,
-    //   label: file,
-    //   release_id: release.data.id,
-    //   data: data,
-    //   baseUrl: 'https://uploads.github.com'
-    // });
-    // console.log('Finished uploading asset', upload.status);
-  }
-  if (pathToAssetWindows) {
-    // win is bundled on linux
-    const pack = await exec(`pnpm oclif pack win`);
-    if (pack.stderr) {
-      throw new Error(`Failed to pack: ${pack.stderr}`);
-    }
-    console.log('Successfully packed CLI', pack.stdout);
-
-    const files = fs.readdirSync(pathToAssetWindows);
-
-    for (const file of files) {
-      console.log('file in directory', file);
-      const data = fs.readFileSync(pathToAssetWindows + `/${file}`);
-      console.log('data from win32...', data);
-    }
-  }
 }
 
 main();
