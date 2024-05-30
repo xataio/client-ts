9cece90450a8600b7e05173498680ecbafc81a00
Alexis Rico <sferadev@gmail.com>
2024-05-30 17:51:21 +0200

    [Kysely] Add consistency to client initialization
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/plugin-client-kysely/src/driver.ts
+++ packages/plugin-client-kysely/src/driver.ts
@@ -15,6 +15,13 @@ import { SQLPluginResult } from '@xata.io/client';
 
 export type XataDialectConfig = {
   xata: { sql: SQLPluginResult };
+  options?: {
+    /**
+     * The consistency level to use when reading data.
+     * @default 'strong'
+     */
+    consistency?: 'strong' | 'eventual';
+  };
 };
 
 export class XataDialect implements Dialect {
@@ -80,7 +87,11 @@ export class XataConnection implements DatabaseConnection {
     const { sql } = this.#config.xata;
     const { sql: statement, parameters } = compiledQuery;
 
-    const { records, warning } = await sql({ statement, params: parameters as any[] });
+    const { records, warning } = await sql({
+      statement,
+      params: parameters as any[],
+      consistency: this.#config.options?.consistency
+    });
     if (warning) {
       console.warn(warning);
     }
