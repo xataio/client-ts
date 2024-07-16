09fd500584664cae2b9fad7c170cabc1286d29a2
Emily <emily.morgan@code.berlin>
2024-07-16 11:53:22 +0200

    var

--- scripts/release-cli-upload.ts
+++ scripts/release-cli-upload.ts
@@ -7,7 +7,7 @@ const exec = util.promisify(execRaw);
 async function main() {
   if (!process.env.MATRIX_OS) throw new Error('MATRIX_OS is not set');
   if (!process.env.PUBLISHED_PACKAGES) throw new Error('PUBLISHED_PACKAGES is not set');
-  if (!process.env.COMMIT) throw new Error('COMMIT is not set');
+  if (!process.env.COMMIT_SHA) throw new Error('COMMIT_SHA is not set');
   if (!process.env.CHANNEL) throw new Error('CHANNEL is not set');
 
   const PATH_TO_CLI = process.cwd() + '/cli';
@@ -20,7 +20,7 @@ async function main() {
   const platform = matrixToOclif(process.env.MATRIX_OS);
   exec(`pnpm oclif upload ${platform}`);
   exec(
-    `pnpm oclif promote --${platform} --sha=${process.env.COMMIT} --indexes --version=${version} --channel=${process.env.CHANNEL}`
+    `pnpm oclif promote --${platform} --sha=${process.env.COMMIT_SHA} --indexes --version=${version} --channel=${process.env.CHANNEL}`
   );
 }
 
