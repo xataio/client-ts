5842e8c27991d7e434e84343db44e017d9adbb36
Divyendu Singh <divyendu@xata.io>
2024-07-23 17:14:19 +0200

    feat(multi-schema): add test for start failed in status command

--- cli/src/commands/migration/status.test.ts
+++ cli/src/commands/migration/status.test.ts
@@ -103,6 +103,25 @@ const fetchRollback_CompletedStatus = (url: string, request: any) => {
   }
 };
 
+const fetchStart_FailedStatus = (url: string, request: any) => {
+  if (url === `${baseUrl}/migrations/status` && request.method === 'GET') {
+    return {
+      ok: true,
+      json: async () => {
+        return {
+          completedAt: '2024-07-23T15:10:57.363168Z',
+          error: 'migration is invalid: column "text" does not exist on table "table"',
+          jobID: 'mig_job_cqfsg01ear3lb815tljg',
+          status: 'failed',
+          type: 'start'
+        };
+      }
+    };
+  } else {
+    return baseFetch(url, request);
+  }
+};
+
 promptsMock.mockReturnValue({ confirm: true, database: 'db1', workspace: 'test-1234' });
 
 describe('migration status', () => {
@@ -154,6 +173,27 @@ describe('migration status', () => {
     expect(printTable).toHaveBeenCalledWith(
       ['Job ID', 'Type', 'Job Status', 'Migration Status', 'Completed At'],
       [['mig_job_cqf8vvk40ertajao7270', 'rollback', 'completed', 'completed', '2024-07-22T16:59:46.41859Z']]
+    );
+  });
+
+  test('correctly prints the status, if last migration was command start and the job failed', async () => {
+    const config = await Config.load();
+    const command = new MigrationStatus(['main'], config);
+    const printTable = vi.spyOn(command, 'printTable');
+    fetchMock.mockImplementation(fetchStart_FailedStatus);
+    await command.run();
+    expect(printTable).toHaveBeenCalledWith(
+      ['Job ID', 'Type', 'Job Status', 'Migration Status', 'Completed At', 'Error'],
+      [
+        [
+          'mig_job_cqfsg01ear3lb815tljg',
+          'start',
+          'failed',
+          'failed',
+          '2024-07-23T15:10:57.363168Z',
+          'migration is invalid: column "text" does not exist on table "table"'
+        ]
+      ]
     );
   });
 });
