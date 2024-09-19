b57e12bc78160a2ab0af2f7c45358cadaada8517
Alexis Rico <sferadev@gmail.com>
2024-09-19 11:17:53 +0200

    Misc updates
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/cli/.eslintrc.cjs
+++ packages/cli/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: 'cli/tsconfig.json'
+    project: 'packages/cli/tsconfig.json'
   },
   rules: {
     '@typescript-eslint/no-floating-promises': 'error',
