bae3eceef7349732acdc4fe8642a98258b40b872
Alexis Rico <sferadev@gmail.com>
2025-03-12 14:53:40 +0100

    Add shebang
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/pgroll/src/mcp.ts
+++ packages/pgroll/src/mcp.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { generateJSONSchema, PgRollMigrationDefinition } from '.';
