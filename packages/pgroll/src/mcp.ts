ceb1f12245ca6fb6482ba1e9f77688866f29d532
Alexis Rico <sferadev@gmail.com>
2025-03-12 16:49:00 +0100

    Add examples
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/pgroll/src/mcp.ts
+++ packages/pgroll/src/mcp.ts
@@ -17,6 +17,24 @@ server.resource('json-schema', new ResourceTemplate('pgroll://schema', { list: u
   ]
 }));
 
+server.resource('list-examples', new ResourceTemplate('pgroll://examples', { list: undefined }), async (uri) => {
+  const response = await fetch(`https://raw.githubusercontent.com/xataio/pgroll/refs/heads/main/examples/.ledger`);
+  const text = await response.text();
+  return { contents: [{ uri: uri.href, text }] };
+});
+
+server.resource(
+  'get-example',
+  new ResourceTemplate('pgroll://examples/{exampleName}', { list: undefined }),
+  async (uri, { exampleName }) => {
+    const response = await fetch(
+      `https://raw.githubusercontent.com/xataio/pgroll/refs/heads/main/examples/${exampleName}.json`
+    );
+    const text = await response.text();
+    return { contents: [{ uri: uri.href, text }] };
+  }
+);
+
 server.tool('validate-migration', { migration: PgRollMigrationDefinition }, async ({ migration }) => {
   const result = PgRollMigrationDefinition.safeParse(migration);
   return {
